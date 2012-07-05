f0890ef0a7190fa42ba528120d464484b307eff4
Repooc <tjcooper1982@gmail.com>
2012-07-05 15:28:12 -0500

    Added a font I wanted.

---
+++ ElvUI_SLE/media/sharedmedia.lua
@@ -0,0 +1,5 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+if LSM == nil then return end
+
+LSM:Register("font","Accidental Presidency", [[Interface\AddOns\ElvUI_SLE\media\fonts\Accidental_Presidency.ttf]])
