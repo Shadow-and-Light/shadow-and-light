a5ab4d2b7beadc7f0e64d2f420570ce92ac8b74b
Repooc <repooc@tukui.org>
2012-09-30 01:16:49 -0500

    Removed a font as it conflicted with some special characters.
    Signed-off-by: Repooc <repooc@tukui.org>

--- ElvUI_SLE/media/sharedmedia.lua
+++ ElvUI_SLE/media/sharedmedia.lua
@@ -3,4 +3,3 @@ local LSM = LibStub("LibSharedMedia-3.0")
 if LSM == nil then return end
 
 LSM:Register("font","Accidental Presidency", [[Interface\AddOns\ElvUI_SLE\media\fonts\Accidental_Presidency.ttf]])
-LSM:Register("font","Rehotalko", [[Interface\AddOns\ElvUI_SLE\media\fonts\Rehotalko.ttf]])
