74f707aa7b7f63fe32301ede64c467c0bf85bcdd
Darthpred <darthpred@gmail.com>
2013-02-16 20:25:23 +0400

    Blank files for farm ABs

---
+++ ElvUI_SLE/modules/farm/farm.lua
@@ -0,0 +1,1 @@
+﻿local E, L, V, P, G, _ = unpack(ElvUI); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
---
+++ ElvUI_SLE/modules/farm/options.lua
@@ -0,0 +1,2 @@
+﻿local E, L, V, P, G, _ = unpack(ElvUI); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+}
