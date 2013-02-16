7d50bb6277686202b90537da1857eb9aca454b25
Repooc <tjcooper1982@gmail.com>
2013-02-16 11:44:28 -0500

    A test for darth
    Signed-off-by: Repooc <tjcooper1982@gmail.com>

--- ElvUI_SLE/modules/farm/farm.lua
+++ ElvUI_SLE/modules/farm/farm.lua
@@ -1,1 +1,5 @@
 ﻿local E, L, V, P, G, _ = unpack(ElvUI); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+local B = LibStub("LibBabble-SubZone-3.0")
+local BL = B:GetLookupTable()
+
+print(BL["Sunsong Ranch"])
