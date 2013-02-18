96cb839ce87a258537869c86d34c8fff5adac62c
Repooc <tjcooper1982@gmail.com>
2013-02-17 23:47:04 -0500

    Don't need anymore
    Signed-off-by: Repooc <tjcooper1982@gmail.com>

--- ElvUI_SLE/modules/datatexts/options.lua
+++ ElvUI_SLE/modules/datatexts/options.lua
@@ -20,47 +20,6 @@ local chatT = {
 	["Right Chat"] = {"chatright", 10, "rightChatPanel", "RightChat"},
 }
 
-
-function DT:PanelLayoutOptions()	
-	for name, _ in pairs(DT.RegisteredDataTexts) do
-			datatexts[name] = name
-	end
-	datatexts[''] = ''
-	
-	local table = E.Options.args.datatexts.args.panels.args
-	local i = 0
-	for pointLoc, tab in pairs(P.datatexts.panels) do
-		i = i + 1
-		if not _G[pointLoc] then table[pointLoc] = nil; return; end
-		if type(tab) == 'table' then
-			table[pointLoc] = {
-				type = 'group',
-				args = {},
-				name = L[pointLoc] or pointLoc,
-				guiInline = true,
-				order = i + -10,
-			}			
-			for option, value in pairs(tab) do
-				table[pointLoc].args[option] = {
-					type = 'select',
-					name = L[option] or option:upper(),
-					values = datatexts,
-					get = function(info) return E.db.datatexts.panels[pointLoc][ info[#info] ] end,
-					set = function(info, value) E.db.datatexts.panels[pointLoc][ info[#info] ] = value; DT:LoadDataTexts() end,									
-				}
-			end
-		elseif type(tab) == 'string' then
-			table[pointLoc] = {
-				type = 'select',
-				name = L[pointLoc] or pointLoc,
-				values = datatexts,
-				get = function(info) return E.db.datatexts.panels[pointLoc] end,
-				set = function(info, value) E.db.datatexts.panels[pointLoc] = value; DT:LoadDataTexts() end,	
-			}						
-		end
-	end
-end
-
 --Datatext panels
 E.Options.args.sle.args.datatext = {
 	type = "group",
