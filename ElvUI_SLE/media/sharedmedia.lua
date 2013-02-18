e7c03245c8e31a62554c52e9e4593f50ea6bc9d2
Repooc <tjcooper1982@gmail.com>
2013-02-17 23:37:28 -0500

    Some more updates.
    Signed-off-by: Repooc <tjcooper1982@gmail.com>

--- ElvUI_SLE/media/sharedmedia.lua
+++ ElvUI_SLE/media/sharedmedia.lua
@@ -3,3 +3,4 @@ local LSM = LibStub("LibSharedMedia-3.0")
 if LSM == nil then return end
 
 LSM:Register("font","Accidental Presidency", [[Interface\AddOns\ElvUI_SLE\media\fonts\Accidental_Presidency.ttf]])
+LSM:Register("statusbar","Polished Wood", [[Interface\AddOns\ElvUI_SLE\media\textures\bar15.tga]])
