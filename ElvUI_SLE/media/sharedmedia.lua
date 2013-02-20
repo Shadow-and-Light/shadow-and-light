a74001d177e029605a2ef13ddfd46c3e8e622ceb
Repooc <tjcooper1982@gmail.com>
2013-02-19 19:21:35 -0500

    Add a font for affinitii layout.
    Signed-off-by: Repooc <tjcooper1982@gmail.com>

--- ElvUI_SLE/media/sharedmedia.lua
+++ ElvUI_SLE/media/sharedmedia.lua
@@ -3,4 +3,5 @@ local LSM = LibStub("LibSharedMedia-3.0")
 if LSM == nil then return end
 
 LSM:Register("font","Accidental Presidency", [[Interface\AddOns\ElvUI_SLE\media\fonts\Accidental_Presidency.ttf]])
+LSM:Register("font","Doris PP", [[Interface\AddOns\ElvUI_SLE\media\fonts\DORISBR.TTF]])
 LSM:Register("statusbar","Polished Wood", [[Interface\AddOns\ElvUI_SLE\media\textures\bar15.tga]])
