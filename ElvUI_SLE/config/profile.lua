2c2651c4ef76a389f3075cacd512f7c45a342b65
Darthpred <darthpred@gmail.com>
2013-02-21 22:04:51 +0400

    Updated general profile to Elv's

--- ElvUI_SLE/config/profile.lua
+++ ElvUI_SLE/config/profile.lua
@@ -175,7 +175,6 @@ P['sle'] = {
 --Core
 P['general'] = {
 	["taintLog"] = false,
-	["autoscale"] = true,
 	["stickyFrames"] = true,
 	['loginmessage'] = true,
 	["interruptAnnounce"] = "NONE",
@@ -183,14 +182,14 @@ P['general'] = {
 	['autoRoll'] = false,
 	['vendorGrays'] = false,
 	['autoAcceptInvite'] = false,
-	['bottomPanel'] = false,
+	['bottomPanel'] = true,
 
 	["fontSize"] = 12,
 	["font"] = "ElvUI Font",
 
 	["bordercolor"] = { r = 0.1,g = 0.1,b = 0.1 },
 	["backdropcolor"] = { r = 0.1,g = 0.1,b = 0.1 },
-	["backdropfadecolor"] = { r = .054,g = .054,b = .054, a = 0.8 },
+	["backdropfadecolor"] = { r = .06,g = .06,b = .06, a = 0.8 },
 	["valuecolor"] = {r = 23/255,g = 132/255,b = 209/255},
 	
 	['mapAlpha'] = 1,
