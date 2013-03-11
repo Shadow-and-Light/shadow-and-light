347005ff8a07e748529a31d813d21e6ee6c07325
Darthpred <darthpred@gmail.com>
2013-03-11 06:55:38 +0400

    Update lib

--- ElvUI_SLE/libs/LibElvUIPlugin-1.0/LibElvUIPlugin-1.0.lua
+++ ElvUI_SLE/libs/LibElvUIPlugin-1.0/LibElvUIPlugin-1.0.lua
@@ -1,6 +1,6 @@
 if not ElvUI then return end
 
-local MAJOR, MINOR = "LibElvUIPlugin-1.0", 9
+local MAJOR, MINOR = "LibElvUIPlugin-1.0", 10
 local lib, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 
 
@@ -12,8 +12,24 @@ lib.index = 0
 --
 
 local E = ElvUI[1]
-local L = ElvUI[2]
 local _
+
+-- MULTI Language Support (Default Language: English)
+local MSG_OUTDATED = "Your version of %s is out of date (latest is version %d). You can download the latest version from http://www.tukui.org"
+local HDR_CONFIG = "Plugins"
+local HDR_INFORMATION = "LibElvUIPlugin-1.0.%d - Plugins Loaded  (Green means you have current version, Red means out of date)"
+local INFO_BY = "by"
+local INFO_VERSION = "Version:"
+local INFO_NEW = "Newest:"
+
+if GetLocale() == "ruRU" then -- Russian Translations
+	MSG_OUTDATED = "Ваша версия %s устарела. Вы можете скачать последнюю версию на http://www.tukui.org"
+	HDR_CONFIG = "Плагины"
+	HDR_INFORMATION = "LibElvUIPlugin-1.0.%d - загруженные плагины (зеленый означает, что у вас последняя версия, красный - устаревшая)"
+	INFO_BY = "от"
+	INFO_VERSION = "Версия:"
+	INFO_NEW = "Последняя:"
+end
 
 --
 -- Plugin table format:
@@ -81,41 +97,43 @@ function lib:SetupVersionCheck(plugin)
 		end
 	end
 	RegisterAddonMessagePrefix(prefix)
-	local function SendRecieve(self, event, mprefix, message, channel, sender)
-		if event == "CHAT_MSG_ADDON" then
-			if sender == E.myname or not sender or mprefix ~= prefix  or plugin.name == MAJOR then return end
-			
-			if not E[plugin.name.."recievedOutOfDateMessage"] then
-				if plugin.version ~= 'BETA' and tonumber(message) ~= nil and tonumber(message) > tonumber(plugin.version) then
-					plugin.old = true
-					plugin.newversion = tonumber(message)
-					local Pname = GetAddOnMetadata(plugin.name, "Title")
-					E:Print(L["Your version of "] .. Pname .. L[" is out of date. You can download the latest version from http://www.tukui.org"])
-					E[plugin.name.."recievedOutOfDateMessage"] = true
+	local function SendRecieve(prefix)
+		return function(self, event, mprefix, message, channel, sender)
+			if event == "CHAT_MSG_ADDON" then
+				if sender == E.myname or not sender or mprefix ~= prefix  or plugin.name == MAJOR then return end
+				
+				if not E[plugin.name.."recievedOutOfDateMessage"] then
+					if plugin.version ~= 'BETA' and tonumber(message) ~= nil and tonumber(plugin.version) ~= nil and tonumber(message) > tonumber(plugin.version) then
+						plugin.old = true
+						plugin.newversion = tonumber(message)
+						local Pname = GetAddOnMetadata(plugin.name, "Title")
+						E:Print(format(MSG_OUTDATED,Pname,plugin.newversion))
+						E[plugin.name.."recievedOutOfDateMessage"] = true
+					end
 				end
+			else
+				E["Send"..plugin.name.."MSGTimer"] = E:ScheduleTimer("Send"..plugin.name.."VersionCheck", 12)
 			end
-		else
-			E["Send"..plugin.name.."MSGTimer"] = E:ScheduleTimer("Send"..plugin.name.."VersionCheck", 12)
 		end
 	end
 
 	local f = CreateFrame('Frame')
 	f:RegisterEvent("GROUP_ROSTER_UPDATE")
 	f:RegisterEvent("CHAT_MSG_ADDON")
-	f:SetScript('OnEvent', SendRecieve)
+	f:SetScript('OnEvent', SendRecieve(prefix))
 end
 
 function lib:GetPluginOptions()
 	E.Options.args.plugins = {
         order = 10000,
         type = "group",
-        name = L["Plugins"],
+        name = HDR_CONFIG,
         guiInline = false,
         args = {
             pluginheader = {
                 order = 1,
                 type = "header",
-                name = "LibElvUIPlugin-1.0."..MINOR..L[" - Plugins Loaded  (Green means you have current version, Red means out of date)"],
+                name = format(HDR_INFORMATION, MINOR),
             },
             plugins = {
                 order = 2,
@@ -132,15 +150,15 @@ function lib:GeneratePluginList()
 	for _, plugin in pairs(lib.plugins) do
 		if plugin.name ~= MAJOR then
 			local author = GetAddOnMetadata(plugin.name, "Author")
-			local Pname = GetAddOnMetadata(plugin.name, "Title")
+			local Pname = GetAddOnMetadata(plugin.name, "Title") or plugin.name
 			local color = plugin.old and E:RGBToHex(1,0,0) or E:RGBToHex(0,1,0)
 			list = list .. Pname 
 			if author then
-			  list = list .. " "..L["by"].." " .. author
+			  list = list .. " ".. INFO_BY .." " .. author
 			end
-			list = list .. color .. " - " ..L["Version"].." " .. plugin.version
+			list = list .. color .. " - " .. INFO_VERSION .." " .. plugin.version
 			if plugin.old then
-			  list = list .. L[" (Newest: "] .. plugin.newversion .. ")"
+			  list = list .. INFO_NEW .. plugin.newversion .. ")"
 			end
 			list = list .. "|r\n"
 		end
