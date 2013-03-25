cd2c415897ccf6c219b9b4143261fd3c239706df
Darthpred <darthpred@gmail.com>
2013-03-25 23:09:40 +0400

    derp

--- ElvUI_SLE/modules/farm/farm.lua
+++ ElvUI_SLE/modules/farm/farm.lua
@@ -354,7 +354,7 @@ function F:Zone(event)
 		F:RegisterEvent("BAG_UPDATE", "InventoryUpdate")
 		F:RegisterEvent("BAG_UPDATE_COOLDOWN")
 		F:RegisterEvent("UNIT_QUEST_LOG_CHANGED", "UpdateLayout")
-		F:RegisterEvent("UNIT_SPELLCAST_SUCCESS ", "InventoryUpdate")
+		F:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED", "InventoryUpdate")
 
 		F:InventoryUpdate(event)
 		F:UpdateLayout()
@@ -363,7 +363,7 @@ function F:Zone(event)
 		F:UnregisterEvent("BAG_UPDATE")
 		F:UnregisterEvent("BAG_UPDATE_COOLDOWN")
 		F:UnregisterEvent("UNIT_QUEST_LOG_CHANGED")
-		F:UnregisterEvent("UNIT_SPELLCAST_SUCCESS")
+		F:UnregisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 		if Zcheck then
 			F:UpdateLayout()
 			Zcheck = false
