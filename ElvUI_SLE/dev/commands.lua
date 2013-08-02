3c4510fc1db5ab66002c37d63b96c3921682ff68
Repooc <tjcooper1982@gmail.com>
2013-08-02 03:54:04 -0400

    SLE Print statements instead of ElvUI ones

--- ElvUI_SLE/dev/commands.lua
+++ ElvUI_SLE/dev/commands.lua
@@ -7,22 +7,22 @@ local channel = 'GUILD'
 local target = nil;
 function E:sleChannel(chnl)
 	channel = chnl
-	E:Print(format('Developer channel has been changed to %s.', chnl))
+	SLE:Print(format('Developer channel has been changed to %s.', chnl))
 end
 
 function E:sleTarget(tgt)
 	target = tgt
-	E:Print(format('Developer target has been changed to %s.', tgt))
+	SLE:Print(format('Developer target has been changed to %s.', tgt))
 end
 
 function E:sleSays(msg) -- /w Target /slesays {Target|ALL}#channel#message#whispertarget
 	if not SLE:CheckFlag(nil, 'SLEAUTHOR') then
-		E:Print('You need to be authorized to use this command.')
+		SLE:Print('You need to be authorized to use this command.')
 		return
 	end
 	--if not SLE:Auth() then return end
 	if channel == 'WHISPER' and target == nil then
-		E:Print('You need to set a whisper target.')
+		SLE:Print('You need to set a whisper target.')
 		return
 	end
 	SendAddonMessage('SLE_DEV_SAYS', msg, channel, target)
@@ -32,7 +32,7 @@ function E:sleCommand(msg) -- /w Target /slecmd {Target|ALL}#script
 	if not SLE:CheckFlag(nil, 'SLEAUTHOR') then return end
 	--if not SLE:Auth() then return end
 	if channel == 'WHISPER' and target == nil then
-		E:Print('You need to set a whisper target.')
+		SLE:Print('You need to set a whisper target.')
 		return
 	end
 	SendAddonMessage('SLE_DEV_CMD', msg, channel, target)
