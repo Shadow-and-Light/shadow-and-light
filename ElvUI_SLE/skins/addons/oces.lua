501bdaa33450cb3fb9998200ab76941dbb81540b
Darthpred <darthpred@gmail.com>
2013-11-03 01:48:36 +0400

    Azil added OCES like a thousand years ago

--- ElvUI_SLE/skins/addons/oces.lua
+++
@@ -1,7 +0,0 @@
-local E, L, V, P, G, _ = unpack(ElvUI); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB, Localize Underscore
-local S = E:GetModule('Skins')
-
-local function LoadSkin()
-	S:HandleButton(TradeSkillCreateScrollButton, true)
-end
-S:RegisterSkin('OneClickEnchantScroll', LoadSkin)
