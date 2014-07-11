c82eaa83a117f7738f841884b2d14dbdd3c2afdd
Darthpred <darthpred@gmail.com>
2014-07-11 20:51:32 +0400

    A bunch of other modules

---
+++ ElvUI_SLE/modules/loot.lua
@@ -0,0 +1,141 @@
+﻿local E, L, V, P, G, _ = unpack(ElvUI); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB, Localize Underscore
+-- local LT = E:NewModule('SLE_Loot', 'AceHook-3.0', 'AceEvent-3.0')
+local LT = E:GetModule('SLE_Loot')
+local check = false
+local t = 0
+local loottemp = {}
+local MyName = E.myname
+local IsInGroup, IsInRaid, IsPartyLFG = IsInGroup, IsInRaid, IsPartyLFG
+local GetNumGroupMembers, GetRaidRosterInfo = GetNumGroupMembers, GetRaidRosterInfo
+local GetLootSlotType, GetLootSlotLink, GetLootSlotInfo = GetLootSlotType, GetLootSlotLink, GetLootSlotInfo
+local GetNumLootItems, GetItemInfo = GetNumLootItems, GetItemInfo
+local IsLeftControlKeyDown = IsLeftControlKeyDown
+local loot = {}
+local numbers = {}
+local n = 0
+local Tremove = table.remove
+
+local function Check()
+	local name, rank, isML
+	for x = 1, GetNumGroupMembers() do
+		name, rank, _, _, _, _, _, _, _, _, isML = GetRaidRosterInfo(x)
+		if name == MyName and isML then
+			return true
+		elseif name == MyName and rank == 1 then
+			return true
+		elseif name == MyName and rank == 2 then
+			return true
+		end
+	end
+	return false
+end
+
+local function Merge()
+	local p, k
+	for i = 1, #loot do
+		k = 1
+		while loot[i] ~= loot[k] do k = k + 1 end
+		if i ~= k then
+			numbers[i] = numbers[i] + numbers[k]
+			Tremove(numbers, k)
+			Tremove(loot, k)
+			n = n - 1
+		end
+	end
+end
+
+local function PopulateTable(q)
+	local p, k
+	for i = 1, GetNumLootItems() do
+		if GetLootSlotType(i) == 1 then 
+			local _, item, quantity, quality = GetLootSlotInfo(i)
+			local link, ilvl
+
+			if quality >= q then
+				link = GetLootSlotLink(i)
+				ilvl = select(4, GetItemInfo(link))
+				
+				n = n + 1
+				loot[n] = link
+				loot[n] = loot[n].." (ilvl: "..ilvl..")"
+				numbers[n] = quantity
+			end
+		end
+	end
+	Merge()
+end
+
+local function Channel()
+	local channel
+	if E.db.sle.loot.chat ~= "SAY" and IsPartyLFG() then
+		return "INSTANCE_CHAT"
+	end
+	if E.db.sle.loot.chat == "RAID" and not IsInRaid() then
+		return "PARTY"
+	end
+	return E.db.sle.loot.chat
+end
+
+local function List()
+	for i = 1, n do
+		if numbers[i] == 1 then
+			SendChatMessage(i..". "..loot[i], Channel())
+		elseif numbers[i] > 1 then
+			SendChatMessage(i..". "..numbers[i].."x"..loot[i], Channel())
+		end
+		if i == n then 
+			loot = {}
+			numbers = {}
+			n = 0
+		end
+	end
+end
+
+function LT:Announce()
+	if not IsInGroup() then return end -- not in group, exit.
+	local m = 0
+	local q = E.db.sle.loot.quality == "EPIC" and 4 or E.db.sle.loot.quality == "RARE" and 3 or E.db.sle.loot.quality == "UNCOMMON" and 2
+	
+	if (Check() and E.db.sle.loot.auto) or (IsLeftControlKeyDown() and (IsInGroup() or IsInRaid())) then
+		for i = 1, GetNumLootItems() do
+			if GetLootSlotType(i) == 1 then
+				for j = 1, t do
+					if GetLootSlotLink(i) == loottemp[j] then
+						check = true
+					end
+				end 
+			end
+		end
+		if check == false or IsLeftControlKeyDown() then
+			PopulateTable(q)
+			if n ~= 0 then
+				SendChatMessage(L["Loot Dropped:"], Channel())
+				List()
+			end
+		end
+		for i = 1, GetNumLootItems() do
+			if GetLootSlotType(i) == 1 then
+				loottemp[i] = GetLootSlotLink(i)
+			end
+		end
+		t = GetNumLootItems()
+		check = false
+	end
+end
+
+function LT:LootShow()
+	local instance = IsInInstance()
+	LootHistoryFrame:SetAlpha(E.db.sle.lootalpha or 1)
+
+	if (not instance and E.db.sle.lootwin) then
+		LootHistoryFrame:Hide()
+	end
+end
+
+function LT:Initialize()
+	self:RegisterEvent('PLAYER_ENTERING_WORLD', 'LootShow');
+	if not E.private.sle.loot.enable then return end
+	self:RegisterEvent("LOOT_OPENED", "Announce")
+end
+
+-- E:RegisterModule(LT:GetName())
---
+++ ElvUI_SLE/modules/tooltip.lua
@@ -0,0 +1,67 @@
+local E, L, V, P, G, _ = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+local TT = E:GetModule('Tooltip');
+
+--Defaults (Need to be moved.)
+P['tooltip']['mouseOffsetX'] = 0
+P['tooltip']['mouseOffsetY'] = 0
+P['tooltip']['overrideCombat'] = false
+
+local iconPath = [[Interface\AddOns\ElvUI_SLE\media\textures\]]
+
+TT.GameTooltip_SetDefaultAnchorSLE = TT.GameTooltip_SetDefaultAnchor
+function TT:GameTooltip_SetDefaultAnchor(tt, parent)
+	TT:GameTooltip_SetDefaultAnchorSLE(tt, parent)
+	if E.private["tooltip"].enable ~= true then return end
+	if(tt:GetAnchorType() ~= "ANCHOR_CURSOR") then return end
+	if InCombatLockdown() and self.db.visibility.combat then
+		tt:Hide()
+		return
+	end
+	if(parent) then
+		if(self.db.cursorAnchor) then
+			TT:AnchorFrameToMouse(tt);
+			return  
+		end
+	end
+end
+
+function TT:AnchorFrameToMouse(frame)
+	if frame:GetAnchorType() ~= "ANCHOR_CURSOR" then return end
+	--if (E.db.tooltip.onlyMod and not (IsShiftKeyDown() or IsControlKeyDown() or IsAltKeyDown())) then return end
+	local x, y = GetCursorPosition();
+	local scale = frame:GetEffectiveScale();
+	local tipWidth = frame:GetWidth();
+	frame:ClearAllPoints();
+	frame:SetPoint("BOTTOMLEFT", UIParent, "BOTTOMLEFT", (x/scale + (E.db.tooltip.mouseOffsetX - tipWidth/2)), (y/scale + E.db.tooltip.mouseOffsetY));
+end
+
+
+
+TT.GameTooltip_OnTooltipSetUnitSLE = TT.GameTooltip_OnTooltipSetUnit
+function TT:GameTooltip_OnTooltipSetUnit(tt)
+	TT:GameTooltip_OnTooltipSetUnitSLE(tt)
+	if not E.db.sle.tooltipicon then return end
+	local unit = select(2, tt:GetUnit())
+	if(UnitIsPlayer(unit)) then
+		local text = GameTooltipTextLeft1:GetText()
+		local faction = UnitFactionGroup(unit)
+		if not faction then faction = "Neutral" end
+		GameTooltipTextLeft1:SetText("|T"..iconPath..faction..".blp:15:15:0:0:64:64:2:56:2:56|t "..text)
+	end
+end
+
+function TT:AddonName_OnUpdate(self, elapsed)
+  TT:AnchorFrameToMouse(self);
+end
+
+--function TT:MODIFIER_STATE_CHANGED(event, key)
+	--if InCombatLockdown() and E.db.tooltip.combathide and not (E.db.tooltip.overrideCombat and IsShiftKeyDown()) then
+	--		GameTooltip:Hide()
+	--end
+--end
+
+TT.InitializeSLE = TT.Initialize
+function TT:Initialize()
+	TT:InitializeSLE()
+	self:HookScript(GameTooltip, "OnUpdate", "AddonName_OnUpdate");
+end
