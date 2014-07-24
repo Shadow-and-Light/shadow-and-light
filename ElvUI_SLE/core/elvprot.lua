48a257758d8f70aa74e67e56b47df944a6ed3bc2
Darthpred <darthpred@gmail.com>
2014-07-24 22:48:36 +0400

    elvprot hooking

--- ElvUI_SLE/core/elvprot.lua
+++ ElvUI_SLE/core/elvprot.lua
@@ -12,48 +12,48 @@ function E:IsFoolsDay()
 	end
 end
 
-E.BeginFoolsDayEventSLE = E.BeginFoolsDayEvent
-function E:BeginFoolsDayEvent()
-	E.BeginFoolsDayEventSLE(self)
-	tinsert(self.massiveShakeObjects, M1)
-	tinsert(self.massiveShakeObjects, M2)
-	tinsert(self.massiveShakeObjects, M3)
-	tinsert(self.massiveShakeObjects, M4)
-	tinsert(self.massiveShakeObjects, M5)
-	tinsert(self.massiveShakeObjects, M6)
-	tinsert(self.massiveShakeObjects, M7)
-	tinsert(self.massiveShakeObjects, M8)
+local function Fools2013()
+	tinsert(E.massiveShakeObjects, M1)
+	tinsert(E.massiveShakeObjects, M2)
+	tinsert(E.massiveShakeObjects, M3)
+	tinsert(E.massiveShakeObjects, M4)
+	tinsert(E.massiveShakeObjects, M5)
+	tinsert(E.massiveShakeObjects, M6)
+	tinsert(E.massiveShakeObjects, M7)
+	tinsert(E.massiveShakeObjects, M8)
 	for i=1, 5 do
 		if FseedButtons[i] then
 			for j = 1, #FseedButtons[i] do
-					tinsert(self.massiveShakeObjects, FseedButtons[i][j])
+					tinsert(E.massiveShakeObjects, FseedButtons[i][j])
 			end
 		end
 	end
 	for i = 1, #FtoolButtons do
-		tinsert(self.massiveShakeObjects, FtoolButtons[i])
+		tinsert(E.massiveShakeObjects, FtoolButtons[i])
 	end
 	for i = 1, #FportalButtons do
-		tinsert(self.massiveShakeObjects, FportalButtons[i])
+		tinsert(E.massiveShakeObjects, FportalButtons[i])
 	end
-	tinsert(self.massiveShakeObjects, ConfigUIButton)
-	tinsert(self.massiveShakeObjects, ReloadUIButton)
-	tinsert(self.massiveShakeObjects, MoveUIButton)
-	tinsert(self.massiveShakeObjects, Bbutton)
-	tinsert(self.massiveShakeObjects, Abutton)
-	if Fbutton then tinsert(self.massiveShakeObjects, Fbutton) end
-	tinsert(self.massiveShakeObjects, DP_1)
-	tinsert(self.massiveShakeObjects, DP_2)
-	tinsert(self.massiveShakeObjects, DP_3)
-	tinsert(self.massiveShakeObjects, DP_4)
-	tinsert(self.massiveShakeObjects, DP_5)
-	tinsert(self.massiveShakeObjects, DP_6)
-	tinsert(self.massiveShakeObjects, Top_Center)
-	tinsert(self.massiveShakeObjects, Bottom_Panel)
-	tinsert(self.massiveShakeObjects, BottomBG)
-	tinsert(self.massiveShakeObjects, LeftBG)
-	tinsert(self.massiveShakeObjects, RightBG)
-	tinsert(self.massiveShakeObjects, ActionBG)
-	tinsert(self.massiveShakeObjects, RaidUtility_ShowButton)
-	tinsert(self.massiveShakeObjects, RaidUtilityPanel)
+	tinsert(E.massiveShakeObjects, ConfigUIButton)
+	tinsert(E.massiveShakeObjects, ReloadUIButton)
+	tinsert(E.massiveShakeObjects, MoveUIButton)
+	tinsert(E.massiveShakeObjects, Bbutton)
+	tinsert(E.massiveShakeObjects, Abutton)
+	if Fbutton then tinsert(E.massiveShakeObjects, Fbutton) end
+	tinsert(E.massiveShakeObjects, DP_1)
+	tinsert(E.massiveShakeObjects, DP_2)
+	tinsert(E.massiveShakeObjects, DP_3)
+	tinsert(E.massiveShakeObjects, DP_4)
+	tinsert(E.massiveShakeObjects, DP_5)
+	tinsert(E.massiveShakeObjects, DP_6)
+	tinsert(E.massiveShakeObjects, Top_Center)
+	tinsert(E.massiveShakeObjects, Bottom_Panel)
+	tinsert(E.massiveShakeObjects, BottomBG)
+	tinsert(E.massiveShakeObjects, LeftBG)
+	tinsert(E.massiveShakeObjects, RightBG)
+	tinsert(E.massiveShakeObjects, ActionBG)
+	tinsert(E.massiveShakeObjects, RaidUtility_ShowButton)
+	tinsert(E.massiveShakeObjects, RaidUtilityPanel)
 end
+
+hooksecurefunc(E, "BeginFoolsDayEvent", Fools2013)
