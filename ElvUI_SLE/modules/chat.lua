ed6b7f8013c2141c17bb023d2fbbebf6ebcc8f80
Darthpred <darthpred@gmail.com>
2014-10-19 01:33:29 +0400

    Removed the table in chat

--- ElvUI_SLE/modules/chat.lua
+++ ElvUI_SLE/modules/chat.lua
@@ -31,99 +31,7 @@ local rolePaths = {
 	HEALER = [[|TInterface\AddOns\ElvUI\media\textures\healer:15:15:0:0:64:64:2:56:2:56|t]],
 	DAMAGER = [[|TInterface\AddOns\ElvUI\media\textures\dps:15:15|t]]
 }
-
---Chat icon paths
-local adapt = "|TInterface\\AddOns\\ElvUI_SLE\\media\\textures\\adapt:0:2|t"
-local repooc = "|TInterface\\AddOns\\ElvUI_SLE\\media\\textures\\SLE_Chat_Logo:0:2|t"
-local darth = "|TInterface\\AddOns\\ElvUI_SLE\\media\\textures\\SLE_Chat_LogoD:0:2|t"
-local friend = "|TInterface\\Icons\\Spell_Holy_PrayerofSpirit:16:16|t"
-local test = "|TInterface\\Icons\\Achievement_PVP_H_03:16:16|t"
-local rpg = "|TInterface\\AddOns\\ElvUI_SLE\\media\\textures\\Chat_RPG:13:35|t"
-local kitalie = "|TInterface\\Icons\\%s:12:12:0:0:64:64:4:60:4:60|t"
-local leader = [[|TInterface\GroupFrame\UI-Group-LeaderIcon:12:12|t]]
-
-
-local specialChatIcons = {
-	["Spirestone"] = {
-		["Sifupooc"] = repooc,
-		["Dapooc"] = repooc,
-		["Lapooc"] = repooc,
-		["Warpooc"] = repooc,
-		["Repooc"] = repooc,
-		["Cursewordz"] = repooc,
-		--Adapt Roster
-		["Mobius"] = adapt,
-		["Urgfelstorm"] = adapt,
-		["Kilashandra"] = adapt,
-		["Electrro"] = adapt,
-		["Afterthot"] = adapt,
-		["Lavathing"] = adapt,
-		["Finkle"] = adapt,
-		["Chopsti"] = adapt,
-		["Taiin"] = adapt
-	},
-	["Illidan"] = {
-		--Darth's toon
-		["Darthpred"] = darth,
-		--Repooc's Toon
-		["Repøøc"] = repooc,
-		["Repooc"] = repooc
-	},
-	["WyrmrestAccord"] = {
-		["Kìtalie"] = kitalie:format("inv_cloth_challengewarlock_d_01helm"),
-		["Sagome"] = kitalie:format("inv_helm_leather_challengemonk_d_01"),
-		["Ainy"] = kitalie:format("inv_helm_plate_challengedeathknight_d_01"),
-		["Norinael"] = kitalie:format("inv_helmet_plate_challengepaladin_d_01"),
-		["Tritalie"] = kitalie:format("inv_helm_cloth_challengemage_d_01"),
-		["Myùn"] = kitalie:format("inv_helmet_mail_challengeshaman_d_01"),
-		["Nevaleigh"] = kitalie:format("inv_helmet_leather_challengerogue_d_01"),
-		["Celenii"] = kitalie:format("inv_helmet_cloth_challengepriest_d_01"),
-		["Varysa"] = kitalie:format("inv_helmet_mail_challengehunter_d_01"),
-		["Caylasena"] = kitalie:format("inv_helm_plate_challengewarrior_d_01"),
-		["Arillora"] = kitalie:format("inv_helmet_challengedruid_d_01"),
-		["Dapooc"] = repooc,
-	},
-	["СвежевательДуш"] = {
-		--Darth's toons
-		["Большойгном"] = test, --Testing toon
-		["Фергесон"] = friend
-	},
-	["ВечнаяПесня"] = {
-		--Darth's toons
-		["Дартпредатор"] = darth,
-		["Алея"] = darth,
-		["Ваззули"] = darth,
-		["Сиаранна"] = darth,
-		["Джатон"] = darth,
-		["Фикстер"] = darth,
-		["Киландра"] = darth,
-		["Нарджо"] = darth,
-		["Келинира"] = darth,
-		["Крениг"] = darth,
-		["Мейжи"] = darth,
-		--Darth's friends
-		["Леани"] = friend,
-		--Da tester lol
-		["Харореанн"] = test,
-		["Нерререанн"] = test
-	},
-	["Ревущийфьорд"] = {
-		["Рыжая"] = friend,
-		["Рыжа"] = friend,
-		--Some people
-		["Брэгар"] = test
-	},
-	["Азурегос"] = {
-		["Брэгари"] = test
-	},
-	["Andorhal"] = {
-		["Dapooc"] = repooc,
-		["Rovert"] = repooc,
-		["Sliceoflife"] = repooc
-	},
-}
-
-SLE.SpecialChatIcons = specialChatIcons
+local specialChatIcons
 
 local function Style(self, frame)
 	CreatedFrames = frame:GetID()
@@ -380,4 +288,5 @@ hooksecurefunc(CH, "Initialize", function(self)
 		self:RegisterEvent('GUILD_ROSTER_UPDATE', Roster)
 		GMCheck()
 	end
+	specialChatIcons = SLE.SpecialChatIcons[SLE.region]
 end)
