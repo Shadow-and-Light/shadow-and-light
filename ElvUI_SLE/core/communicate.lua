d38040706a4db98d930b11417c2cea96569cb6bf
Darthpred <darthpred@gmail.com>
2014-10-19 01:44:19 +0400

    w/e

--- ElvUI_SLE/core/communicate.lua
+++ ElvUI_SLE/core/communicate.lua
@@ -2,70 +2,6 @@
 local SLE = E:GetModule('SLE');
 local split = string.split
 
---The list of authorized toons
-local Authors = {
-	["ВечнаяПесня"] = {
-		--Darth's toons
-		["Дартпредатор"] = true,
-		["Алея"] = true,
-		["Ваззули"] = true,
-		["Сиаранна"] = true,
-		["Джатон"] = true,
-		["Фикстер"] = true,
-		["Киландра"] = true,
-		["Нарджо"] = true,
-		["Келинира"] = true,
-		["Крениг"] = true,
-		["Мейжи"] = true
-	},
-	["Spirestone"] = {
-		["Sifupooc"] = true,
-		["Dapooc"] = true,
-		["Lapooc"] = true,
-		["Warpooc"] = true,
-		["Repooc"] = true,
-		["Cursewordz"] = true,
-	},
-	["WyrmrestAccord"] = {
-		["Kitalie"] = true,
-		["Sagome"] = true,
-		["Ainy"] = true,
-		["Norinael"] = true,
-		["Tritalie"] = true,
-		["Myun"] = true,
-		["Nevaleigh"] = true,
-		["Celenii"] = true,
-		["Varysa"] = true,
-		["Caylasena"] = true,
-		["Arillora"] = true,
-	},
-	--Normal PTR
-	["Anasterian(US)"] = {
-		["Dapooc"] = true,
-	},
-	["Brill(EU)"] = {
-		["Дартпредатор"] = true,
-	},
-}
-
-function SLE:Auth(sender)
-	local senderName, senderRealm
-
-	if sender then
-		senderName, senderRealm = string.split('-', sender)
-	else
-		senderName = E.myname
-	end
-
-	senderRealm = senderRealm or E.myrealm
-	senderRealm = senderRealm:gsub(' ', '')
-	if Authors[senderRealm] and Authors[senderRealm][senderName] then
-		return Authors[senderRealm][senderName]
-	end
-
-	return false
-end
-
 local function SendRecieve(self, event, prefix, message, channel, sender)
 	if event == "CHAT_MSG_ADDON" then
 		if sender == E.myname.."-"..E.myrealm:gsub(' ','') then return end
@@ -99,7 +35,7 @@ end
 
 RegisterAddonMessagePrefix('SLE_DEV_REQ')
 
-local f = CreateFrame('Frame', "DaFrame")
+local f = CreateFrame('Frame', "SLE_Comm_Frame")
 f:RegisterEvent("GROUP_ROSTER_UPDATE")
 f:RegisterEvent("CHAT_MSG_ADDON")
 f:RegisterEvent("BN_CHAT_MSG_ADDON")
