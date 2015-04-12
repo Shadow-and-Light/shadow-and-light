6f7a63920ee724a3ee3ec40791b26ad22b462232
Darthpred <darthpred@gmail.com>
2015-04-12 11:44:33 +0400

    just in case

--- ElvUI_SLE/modules/characterframe/characterframe.lua
+++ ElvUI_SLE/modules/characterframe/characterframe.lua
@@ -634,7 +634,7 @@ function CA:Update_Gear()
 						if not IsEnchanted and C.EnchantableSlots[SlotName] then
 							local id = GetInventoryItemID("player", Slot.ID)
 							local IType = select(9, GetItemInfo(id))
-							if IType == "INVTYPE_WEAPON" or IType == "INVTYPE_RANGEDRIGHT" or IType == "INVTYPE_2HWEAPON"then 
+							if IType == "INVTYPE_WEAPON" or IType == "INVTYPE_RANGEDRIGHT" or IType == "INVTYPE_2HWEAPON" then 
 								ErrorDetected = true
 								Slot.EnchantWarning:Show()
 								Slot.ItemEnchant:SetText('|cffff0000'..L['Not Enchanted'])
