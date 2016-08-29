3049be6904720f20c0df2653fedbd3c32c13ec65
Darthpred <darthpred@gmail.com>
2016-08-29 20:51:46 +0300

    Account for tooltip text size change. This fixes #311

--- ElvUI_SLE/modules/sledatatexts/friends.lua
+++ ElvUI_SLE/modules/sledatatexts/friends.lua
@@ -65,17 +65,14 @@ local BROADCAST_ICON = "|TInterface\\FriendsFrame\\BroadcastIcon:0|t"
 -- Setup the Title Font. 14
 local ssTitleFont = CreateFont("ssTitleFont")
 ssTitleFont:SetTextColor(1,0.823529,0)
-ssTitleFont:SetFont(GameTooltipText:GetFont(), 14)
 
 -- Setup the Header Font. 12
 local ssHeaderFont = CreateFont("ssHeaderFont")
 ssHeaderFont:SetTextColor(1,0.823529,0)
-ssHeaderFont:SetFont(GameTooltipHeaderText:GetFont(), 12)
 
 -- Setup the Regular Font. 12
 local ssRegFont = CreateFont("ssRegFont")
 ssRegFont:SetTextColor(1,0.823529,0)
-ssRegFont:SetFont(GameTooltipText:GetFont(), 12)
 
 local list_sort = {
 	TOONNAME = function(a, b)
@@ -324,6 +321,8 @@ function LDB.OnEnter(self)
 
 		tooltip:SetBackdropColor(0,0,0,1)
 
+		ssHeaderFont:SetFont(GameTooltipHeaderText:GetFont())
+		ssRegFont:SetFont(GameTooltipText:GetFont())
 		tooltip:SetHeaderFont(ssHeaderFont)
 		tooltip:SetFont(ssRegFont)
 
@@ -334,6 +333,7 @@ function LDB.OnEnter(self)
 
 	local line = tooltip:AddLine()
 	if not E.db.sle.dt.friends.hide_titleline then
+		ssTitleFont:SetFont(GameTooltipText:GetFont())
 		tooltip:SetCell(line, 1, "Shadow & Light Friends", ssTitleFont, "CENTER", 0)
 		tooltip:AddLine(" ")
 	end
