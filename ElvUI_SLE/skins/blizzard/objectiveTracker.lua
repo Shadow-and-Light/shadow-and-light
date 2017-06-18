ee1e4a3e5ed88f02501971e8879986c7ae1b91ce
Darth Predator <darthpred@gmail.com>
2017-06-18 17:37:44 +0300

    Fuck this black box for icons again

--- ElvUI_SLE/skins/blizzard/objectiveTracker.lua
+++ ElvUI_SLE/skins/blizzard/objectiveTracker.lua
@@ -43,7 +43,7 @@ local function skinObjectiveBar(self, block, line)
 	local flare = progressBar.FullBarFlare1
 
 
-	if not progressBar.styled then
+	if not progressBar.sle_skinned then
 		local label = bar.Label
 
 		bar.BarBG:Hide()
@@ -64,6 +64,7 @@ local function skinObjectiveBar(self, block, line)
 		bar.backdrop:Point("TOPLEFT", bar, -1, 1)
 		bar.backdrop:Point("BOTTOMRIGHT", bar, 1, -1)
 		bar:SetFrameStrata('HIGH')
+		bar.newIconBg:SetTemplate("NoBackdrop")
 
 		flare:Hide()
 
@@ -73,7 +74,7 @@ local function skinObjectiveBar(self, block, line)
 		SLE:GetModule("Media").BonusObjectiveBarText = label
 
 		BonusObjectiveTrackerProgressBar_PlayFlareAnim = dummy
-		progressBar.styled = true
+		progressBar.sle_skinned = true
 	end
 
 	bar.IconBG:Hide()
@@ -186,6 +187,7 @@ local function ObjectiveReskin()
 		hooksecurefunc(_G["BONUS_OBJECTIVE_TRACKER_MODULE"], "AddProgressBar", skinObjectiveBar) 
 		-- World Quests can be bonus objective type
 		hooksecurefunc(_G["WORLD_QUEST_TRACKER_MODULE"], "AddProgressBar", skinObjectiveBar)
+
 		-- ProgressBar in the ObjectiveTacker
 		hooksecurefunc(DEFAULT_OBJECTIVE_TRACKER_MODULE, "AddProgressBar", function(self, block, line, questID)
 			local progressBar = self.usedProgressBars[block] and self.usedProgressBars[block][line];
