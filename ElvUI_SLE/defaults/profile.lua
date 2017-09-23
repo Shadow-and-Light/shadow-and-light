ddc052d94af2d263585d87b490cc8c668719649d
Repooc <tjcooper1982@gmail.com>
2017-09-23 19:55:22 -0400

    Fixed S&L Datapanel bug missing panel 3 & 7

--- ElvUI_SLE/defaults/profile.lua
+++ ElvUI_SLE/defaults/profile.lua
@@ -932,7 +932,11 @@ P.datatexts.panels["SLE_DataPanel_2"] = {
 	["middle"] = '',
 	["right"] = '',
 }
-P.datatexts.panels["SLE_DataPanel_3"] = 'Version'
+P.datatexts.panels["SLE_DataPanel_3"] = {
+	["left"] = '',
+	["middle"] = 'Version',
+	["right"] = '',
+}	
 P.datatexts.panels["SLE_DataPanel_4"] = {
 	["left"] = '',
 	["middle"] = '',
@@ -948,7 +952,11 @@ P.datatexts.panels["SLE_DataPanel_6"] = {
 	["middle"] = '',
 	["right"] = '',
 }
-P.datatexts.panels["SLE_DataPanel_7"] = ''
+P.datatexts.panels["SLE_DataPanel_7"] = {
+	["left"] = '',
+	["middle"] = '',
+	["right"] = '',
+}
 P.datatexts.panels["SLE_DataPanel_8"] = {
 	["left"] = '',
 	["middle"] = '',
