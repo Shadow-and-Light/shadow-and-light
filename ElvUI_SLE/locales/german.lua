a765b4f55d7409f5a6e43bd146ab88ddf6168efd
Merathilis <neo0608@gmail.com>
2018-03-08 18:13:53 +0100

    Update 1 german locale. LUL

--- ElvUI_SLE/locales/german.lua
+++ ElvUI_SLE/locales/german.lua
@@ -621,7 +621,7 @@ L["Right click on the location panel will bring up a menu with available options
 L["Custom Width"] = true
 L["By default menu's width will be equal to the location panel width. Checking this option will allow you to set own width."] = true
 L["Justify Text"] = true
-L["Hearthstone Location"] = true
+L["Hearthstone Location"] = "Ruhestein Position"
 L["Show the name on location your Heathstone is bound to."] = true
 L["Only Number"] = true
 L["Horizontal Growth"] = true
