7314858e264c109c9bfb62457f75157617503f0e
Darth Predator <darthpred@gmail.com>
2018-05-04 09:00:50 +0300

    Get player faction on load. Cause E.myfaction is localized and that's not what I need

--- ElvUI_SLE/core/core.lua
+++ ElvUI_SLE/core/core.lua
@@ -21,6 +21,8 @@ local Toolkit = {}
 SLE.elvV = tonumber(E.version)
 SLE.elvR = tonumber(GetAddOnMetadata("ElvUI_SLE", "X-ElvVersion"))
 
+SLE.myfaction = UnitFactionGroup('player')
+
 --Setting up table to unpack. Why? no idea
 Engine[1] = SLE
 Engine[2] = Toolkit
