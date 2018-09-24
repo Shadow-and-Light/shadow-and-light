2be52c9111fca4440041602d941b96869d4477bd
Darth Predator <darthpred@gmail.com>
2018-09-24 19:42:01 +0300

    Comments for datatexts.lua

--- ElvUI_SLE/modules/datatexts.lua
+++ ElvUI_SLE/modules/datatexts.lua
@@ -6,22 +6,31 @@ local _G = _G
 local CreateFrame = CreateFrame
 local FACTION_ALLIANCE, FACTION_HORDE = FACTION_ALLIANCE, FACTION_HORDE
 
-DTP.values = {
-	[1] = {"TOPLEFT", 0, 3},
-	[2] = {"TOP", -((E.eyefinity or E.screenwidth)/5), 3},
-	[3] = {"TOP", 0, 1},
-	[4] = {"TOP", ((E.eyefinity or E.screenwidth)/5), 3},
-	[5] = {"TOPRIGHT", 0, 3},
-	[6] = {"BOTTOM", -((E.eyefinity or E.screenwidth)/6 - 15), 3},
-	[7] = {"BOTTOM", 0, 1},
-	[8] = {"BOTTOM", ((E.eyefinity or E.screenwidth)/6 - 15), 3},
+--Datapanels creation templates. Appropriate info is unpacked by panel id.
+DTP.template = {
+	[1] = { point = "TOPLEFT", x = 0, numSlots = 3},
+	[2] = { point = "TOP", x = -((E.eyefinity or E.screenwidth)/5), numSlots = 3},
+	[3] = { point = "TOP", x = 0, numSlots = 1},
+	[4] = { point = "TOP", x = ((E.eyefinity or E.screenwidth)/5), numSlots = 3},
+	[5] = { point = "TOPRIGHT", x = 0, numSlots = 3},
+	[6] = { point = "BOTTOM", x = -((E.eyefinity or E.screenwidth)/6 - 15), numSlots = 3},
+	[7] = { point = "BOTTOM", x = 0, numSlots = 1},
+	[8] = { point = "BOTTOM", x = ((E.eyefinity or E.screenwidth)/6 - 15), numSlots = 3},
 }
 DTP.Names = {}
+--Table to remember where default Gold DT is
 DTP.GoldCache = {}
 
+--Mouseover functions for panels and datatexts on them
 local function Bar_OnEnter(self)
 	if DTP.db["panel"..self.Num].mouseover then
 		E:UIFrameFadeIn(self, 0.2, self:GetAlpha(), DTP.db["panel"..self.Num].alpha)
+	end
+end
+
+local function Bar_OnLeave(self)
+	if DTP.db["panel"..self.Num].mouseover then
+		E:UIFrameFadeOut(self, 0.2, self:GetAlpha(), 0)
 	end
 end
 
@@ -32,12 +41,6 @@ local function Button_OnEnter(self)
 	end
 end
 
-local function Bar_OnLeave(self)
-	if DTP.db["panel"..self.Num].mouseover then
-		E:UIFrameFadeOut(self, 0.2, self:GetAlpha(), 0)
-	end
-end
-
 local function Button_OnLeave(self)
 	local bar = self:GetParent()
 	if DTP.db["panel"..bar.Num].mouseover then
@@ -45,49 +48,62 @@ local function Button_OnLeave(self)
 	end
 end
 
+--The hook to core DT:LoadDataTexts function
 local OnLoadThrottle = true
 function DTP:LoadDTHook()
-	local SLE_Cur_Selected = false
+	--Is S&L's currency dt set anywhere
+	local IsCurrencyDTSelected = false
+	--Wipe the table. We assume after last settings change there is no default gold DTs
 	T.twipe(DTP.GoldCache)
+	--Going through all registered datapanels
 	for panelName, panel in T.pairs(DT.RegisteredPanels) do
-		for i=1, panel.numPoints do
-			local pointIndex = DT.PointLocation[i]
+		for slot = 1, panel.numPoints do
+			local localtion = DT.PointLocation[slot]
+			--If it is S&L's panel hook scripts for show/hide on mouseover
 			if DTP.Names[panelName] then 
-				panel.dataPanels[pointIndex]:HookScript("OnEnter", Button_OnEnter)
-				panel.dataPanels[pointIndex]:HookScript("OnLeave", Button_OnLeave)
+				panel.dataPanels[localtion]:HookScript("OnEnter", Button_OnEnter)
+				panel.dataPanels[localtion]:HookScript("OnLeave", Button_OnLeave)
 			end
-			for option, value in T.pairs(DT.db.panels) do
-				if value and type(value) == 'table' then
-					if option == panelName and DT.db.panels[option][pointIndex] and DT.db.panels[option][pointIndex] == "Gold" then
-						DTP.GoldCache[panelName] = panel.dataPanels[pointIndex]
-					elseif option == panelName and DT.db.panels[option][pointIndex] and DT.db.panels[option][pointIndex] == "S&L Currency" then
-						SLE_Cur_Selected = true
+			--Searching for gold
+			for settingName, settingValue in T.pairs(DT.db.panels) do
+				if settingValue and T.type(settingValue) == 'table' then --if options for panel exist and it is 2+ slot panel (2 cause DTBars exists)
+					if settingName == panelName and DT.db.panels[settingName][localtion] and DT.db.panels[settingName][localtion] == "Gold" then
+						--if it is current panel and options for it exist and it is gold dt, put the location in da cache
+						DTP.GoldCache[panelName] = panel.dataPanels[localtion]
+					elseif settingName == panelName and DT.db.panels[settingName][localtion] and DT.db.panels[settingName][localtion] == "S&L Currency" then
+						--if it is current panel and options for it exist and it is s&l currency dt, set the flag to true
+						IsCurrencyDTSelected = true
 					end
-				elseif value and type(value) == 'string' and value == "Gold" then
-					if DT.db.panels[option] == "Gold" and option == panelName then
-						DTP.GoldCache[panelName] = panel.dataPanels[pointIndex]
+				elseif settingValue and T.type(settingValue) == 'string' and settingValue == "Gold" then --if options for panel exist and it is 1 slot panel with gold dt
+					if DT.db.panels[settingName] == "Gold" and settingName == panelName then
+						DTP.GoldCache[panelName] = panel.dataPanels[localtion]
 					end
-				elseif value and type(value) == 'string' and value == "S&L Currency" then
-					if DT.db.panels[option] == "Gold" and option == panelName then
-						SLE_Cur_Selected = true
+				elseif settingValue and T.type(settingValue) == 'string' and settingValue == "S&L Currency" then --if options for panel exist and it is 1 slot panel with s&l currency
+					if DT.db.panels[settingName] == "Gold" and settingName == panelName then
+						IsCurrencyDTSelected = true
 					end
 				end
 			end
 		end
 	end
+	--Throttle for the amount of times this message is called. This func is called for every single change in DT options, so having it flood the chat is bad
 	if OnLoadThrottle then
 		OnLoadThrottle = false
-		if SLE_Cur_Selected then
-			for k, v in T.pairs(DTP.GoldCache) do
-				local message = T.format(L["SLE_DT_CURRENCY_WARNING_GOLD"], "|cff1784d1"..L[k].."|r")
+		if IsCurrencyDTSelected then
+			for panelName, datatext in T.pairs(DTP.GoldCache) do
+				--Message about this particular panel having gold dt
+				local message = T.format(L["SLE_DT_CURRENCY_WARNING_GOLD"], "|cff1784d1"..L[panelName].."|r")
 				SLE:ErrorPrint(message)
-				if v then v:UnregisterAllEvents() end
+				--Unregister all events for this gold dt to prevent weird shit on currency
+				if datatext then datatext:UnregisterAllEvents() end
 			end
 		end
+		--1 second reset time should be enough to suppress all exessive automatic calls
 		E:Delay(1, function() OnLoadThrottle = true end)
 	end
 end
 
+--S&L's panel hook scripts for show/hide on mouseover. Currently unused
 function DTP:MouseoverHook()
 	for panelName, panel in T.pairs(DT.RegisteredPanels) do
 		for i=1, panel.numPoints do
@@ -100,64 +116,65 @@ function DTP:MouseoverHook()
 	end
 end
 
-function DTP:CreatePanel(i)
-	local panel = CreateFrame('Frame', "SLE_DataPanel_"..i, E.UIParent)
-	panel.Num = i
+function DTP:CreatePanel(id)
+	local panel = CreateFrame('Frame', "SLE_DataPanel_"..id, E.UIParent)
+	panel.Num = id
 	panel:SetFrameStrata('LOW')
-	panel:Point(DTP.values[i][1], E.UIParent, DTP.values[i][1], DTP.values[i][2], 0); 
-	DT:RegisterPanel(panel, DTP.values[i][3], 'ANCHOR_BOTTOM', 0, -4)
+	panel:Point(DTP.template[id].point, E.UIParent, DTP.template[id].point, DTP.template[id].x, 0); 
+	DT:RegisterPanel(panel, DTP.template[id].numSlots, 'ANCHOR_BOTTOM', 0, -4)
 	panel:SetScript("OnEnter", Bar_OnEnter)
 	panel:SetScript("OnLeave", Bar_OnLeave)
 	panel:Hide()
-	DTP.Names["SLE_DataPanel_"..i] = true
+	DTP.Names["SLE_DataPanel_"..id] = true
 
 	return panel
 end
 
-function DTP:Mouseover(i)
-	if DTP.db["panel"..i].mouseover then
-		self["Panel_"..i]:SetAlpha(0)
+function DTP:Mouseover(id)
+	if DTP.db["panel"..id].mouseover then
+		self["Panel_"..id]:SetAlpha(0)
 	else
-		self["Panel_"..i]:SetAlpha(DTP.db["panel"..i].alpha)
+		self["Panel_"..id]:SetAlpha(DTP.db["panel"..id].alpha)
 	end
 end
 
-function DTP:Size(i)
-	self["Panel_"..i]:Size(DTP.db["panel"..i].width, 20)
+function DTP:Size(id)
+	self["Panel_"..id]:Size(DTP.db["panel"..id].width, 20)
 	DT:UpdateAllDimensions()
 end
 
-function DTP:Toggle(i)
-	if DTP.db["panel"..i].enabled then
-		self["Panel_"..i]:Show()
-		if DTP.db["panel"..i].mouseover then Bar_OnLeave(self["Panel_"..i]) end
-		E:EnableMover(self["Panel_"..i].mover:GetName())
+function DTP:Toggle(id)
+	if DTP.db["panel"..id].enabled then
+		self["Panel_"..id]:Show()
+		if DTP.db["panel"..id].mouseover then Bar_OnLeave(self["Panel_"..id]) end
+		E:EnableMover(self["Panel_"..id].mover:GetName())
 	else
-		self["Panel_"..i]:Hide()
-		E:DisableMover(self["Panel_"..i].mover:GetName())
+		self["Panel_"..id]:Hide()
+		E:DisableMover(self["Panel_"..id].mover:GetName())
 	end
 end
 
-function DTP:PetHide(i)
-	if DTP.db["panel"..i].pethide then
-		E:RegisterPetBattleHideFrames(self["Panel_"..i], E.UIParent, "LOW")
+function DTP:PetHide(id)
+	if DTP.db["panel"..id].pethide then
+		E:RegisterPetBattleHideFrames(self["Panel_"..id], E.UIParent, "LOW")
 	else
-		E:UnregisterPetBattleHideFrames(self["Panel_"..i])
+		E:UnregisterPetBattleHideFrames(self["Panel_"..id])
 	end
 end
 
-function DTP:Template(i)
-	if DTP.db["panel"..i].transparent then
-		self["Panel_"..i]:SetTemplate(DTP.db["panel"..i].noback and "NoBackdrop" or "Transparent")
+function DTP:Template(id)
+	if DTP.db["panel"..id].transparent then
+		self["Panel_"..id]:SetTemplate(DTP.db["panel"..id].noback and "NoBackdrop" or "Transparent")
 	else
-		self["Panel_"..i]:SetTemplate(DTP.db["panel"..i].noback and "NoBackdrop" or "Default", true)
+		self["Panel_"..id]:SetTemplate(DTP.db["panel"..id].noback and "NoBackdrop" or "Default", true)
 	end
 end
 
-function DTP:Alpha(i)
-	self["Panel_"..i]:SetAlpha(DTP.db["panel"..i].alpha)
+function DTP:Alpha(id)
+	self["Panel_"..id]:SetAlpha(DTP.db["panel"..id].alpha)
 end
 
+--Resizing chat panels if datapanels handling was enabled. Also applies alpha setting to those, since I can't pass id on those
 function DTP:ChatResize()
 	_G["LeftChatDataPanel"]:SetAlpha(DTP.db.leftchat.alpha)
 	_G["LeftChatToggleButton"]:SetAlpha(DTP.db.leftchat.alpha)
@@ -177,19 +194,20 @@ function DTP:ChatResize()
 end
 
 function DTP:CreateAndUpdatePanels()
-	for i = 1, 8 do
-		if not self["Panel_"..i] then self["Panel_"..i] = DTP:CreatePanel(i) end
-		DTP:Size(i)
-		DTP:Template(i)
-		if not E.CreatedMovers["SLE_DataPanel_"..i.."_Mover"] then E:CreateMover(self["Panel_"..i], "SLE_DataPanel_"..i.."_Mover", L["SLE_DataPanel_"..i], nil, nil, nil, "ALL,S&L,S&L DT") end
-		DTP:Toggle(i)
-		DTP:PetHide(i)
-		DTP:Alpha(i)
-		DTP:Mouseover(i)
+	for id = 1, 8 do
+		if not self["Panel_"..id] then self["Panel_"..id] = DTP:CreatePanel(id) end
+		DTP:Size(id)
+		DTP:Template(id)
+		if not E.CreatedMovers["SLE_DataPanel_"..id.."_Mover"] then E:CreateMover(self["Panel_"..id], "SLE_DataPanel_"..id.."_Mover", L["SLE_DataPanel_"..id], nil, nil, nil, "ALL,S&L,S&L DT") end
+		DTP:Toggle(id)
+		DTP:PetHide(id)
+		DTP:Alpha(id)
+		DTP:Mouseover(id)
 	end
 	DTP:ChatResize()
 end
 
+--This deletes gold data for selected character. Called from config.
 function DTP:DeleteCurrencyEntry(data)
 	if ElvDB['gold'][data.realm] and ElvDB['gold'][data.realm][data.name] then
 		ElvDB['gold'][data.realm][data.name] = nil;
@@ -219,17 +237,22 @@ function DTP:Initialize()
 	end
 
 	DTP:ForUpdateAll()
-	--Datatexts
+
+	--Hooking to default DTs for additional features
 	DTP:HookTimeDT()
 	DTP:HookDurabilityDT()
+
+	--Creating specifing DTs
 	DTP:CreateMailDT()
 	DTP:CreateCurrencyDT()
+	--Replacing OnClick function for SpecSwitch DT
 	DTP:ReplaceSpecSwitch()
 
-	--Remove char
+	--Finishing setup for delete character gold data popup
 	local popup = E.PopupDialogs['SLE_CONFIRM_DELETE_CURRENCY_CHARACTER']
 	popup.OnAccept = DTP.DeleteCurrencyEntry,
 	
+	--Hooking to default functions to remove gold conflicts
 	-- hooksecurefunc(DT, "LoadDataTexts", DTP.MouseoverHook)
 	hooksecurefunc(DT, "LoadDataTexts", DTP.LoadDTHook)
 	hooksecurefunc(MM, "UpdateSettings", DTP.LoadDTHook)
