691e890058485bfae75d7b5fc88f6ad908f5d685
Darth Predator <darthpred@gmail.com>
2018-10-25 13:28:45 +0300

    Some comments

--- ElvUI_SLE/modules/raidroleicons.lua
+++ ElvUI_SLE/modules/raidroleicons.lua
@@ -5,18 +5,22 @@ local PLAYER_REALM = T.gsub(E.myrealm,'[%s%-]','')
 local _G = _G
 local RaiseFrameLevel = RaiseFrameLevel
 
+--Actually make dem icons
 function B:CreateAndUpdateIcons()
 	if not SLE.initialized then return end
 	local members = T.GetNumGroupMembers()
 	for i = 1, members do
 		local frame = _G["RaidGroupButton"..i]
+		--if the frame doesn't exist then wait a bit
 		if not frame or (frame and not frame.subframes) or not E.db.sle.raidmanager then E:Delay(1, B.CreateAndUpdateIcons); return end
+		--if level is shown then attach icons there, otherwise to class
 		local parent = E.db.sle.raidmanager.level and frame.subframes.level or frame.subframes.class
 		if E.db.sle.raidmanager.level then
 			frame.subframes.level:Show()
 		else
 			frame.subframes.level:Hide()
 		end
+		--if the icon is not created for this frame then make that shit
 		if not frame.sleicon then
 			frame.sleicon = CreateFrame("Frame", nil, frame)
 			frame.sleicon:SetSize(14, 14)
@@ -26,6 +30,7 @@ function B:CreateAndUpdateIcons()
 			frame.sleicon.texture:SetAllPoints(frame.sleicon)
 		end
 		frame.sleicon:SetPoint("RIGHT", parent, "LEFT", 2, 0)
+		--Deciding on what texture to use
 		local unit = T.IsInRaid() and "raid" or "party"
 		local role = T.UnitGroupRolesAssigned(unit..i)
 		local name, realm = T.UnitName(unit..i)
@@ -38,6 +43,7 @@ function B:CreateAndUpdateIcons()
 	end
 end
 
+--When raid manager is loaded. do stuff
 function B:RaidLoaded(event, addon)
 	if addon == "Blizzard_RaidUI" then
 		B:CreateAndUpdateIcons()
@@ -46,6 +52,7 @@ function B:RaidLoaded(event, addon)
 	end
 end
 
+--if oRA is not present, then we are doing shit
 if not SLE._Compatibility["oRA3"] then
 	B:RegisterEvent("ADDON_LOADED", "RaidLoaded")
 end
