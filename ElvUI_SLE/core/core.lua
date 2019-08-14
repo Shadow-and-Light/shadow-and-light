2ef8496c8579898b9837f4254ce8ff5b3a643398
Darth Predator <darthpred@gmail.com>
2019-08-14 06:40:44 +0300

    This should fix some stuff not properly localized

--- ElvUI_SLE/core/core.lua
+++ ElvUI_SLE/core/core.lua
@@ -1,4 +1,5 @@
-﻿local E, L, V, P, G = unpack(ElvUI);
+﻿local E, _, V, P, G = unpack(ElvUI);
+local L = E.Libs.ACL:GetLocale('ElvUI', E.global.general.locale or 'enUS')
 local EP = LibStub("LibElvUIPlugin-1.0")
 local AddOnName, Engine = ...;
 local _G = _G
