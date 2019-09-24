251fd4be67ac0ae9b5546216579577c5fe15bc17
Repooc <tjcooper1982@gmail.com>
2019-09-24 00:52:43 -0400

    Some formatting changes

--- ElvUI_SLE/modules/armory/inspect.lua
+++ ElvUI_SLE/modules/armory/inspect.lua
@@ -1,7 +1,7 @@
 local SLE, T, E, L, V, P, G = unpack(select(2, ...))
 if select(2, GetAddOnInfo('ElvUI_KnightFrame')) and IsAddOnLoaded('ElvUI_KnightFrame') then return end --Don't break korean code :D
 local Armory = SLE:GetModule("Armory_Core")
-local IA = SLE:NewModule('Armory_Inspect', 'AceEvent-3.0', 'AceConsole-3.0', 'AceHook-3.0');
+local IA = SLE:NewModule("Armory_Inspect", "AceEvent-3.0", "AceConsole-3.0", "AceHook-3.0");
 
 function IA:ToggleArmory()
 end
