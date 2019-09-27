746ea31dc494a2393aba6aa81882150e55b58af4
Darth Predator <darthpred@gmail.com>
2019-09-27 03:31:52 +0300

    Remove unused variables

--- ElvUI_SLE/defaults/profile.lua
+++ ElvUI_SLE/defaults/profile.lua
@@ -249,13 +249,11 @@ P["sle"] = {
 			["enabled"] = true,
 			["width"] = 396,
 			["alpha"] = 1,
-			["noborders"] = false,
 		},
 		["rightchat"] = {
 			["enabled"] = true,
 			["width"] = 396,
 			["alpha"] = 1,
-			["noborders"] = false,
 		},
 		["chathandle"] = false,
 	},
