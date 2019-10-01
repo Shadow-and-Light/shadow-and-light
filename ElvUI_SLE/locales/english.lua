1b7646bb3a9ff914c6b129f1b14da04c327fdd48
Repooc <tjcooper1982@gmail.com>
2019-10-01 01:22:31 -0400

    Updated Patreon List

--- ElvUI_SLE/locales/english.lua
+++ ElvUI_SLE/locales/english.lua
@@ -1001,7 +1001,8 @@ L["ELVUI_SLE_PATREON_TITLE"] = "Many thanks to our awesome patrons, who provided
 L["Patrons"] = true
 L["Donors"] = true
 L["ELVUI_SLE_PATRONS"] = [[Ali A
-Andre Edwards]]
+Andre Edwards
+Peter aka Pete]]
 
 L["SLE_BENIK_AF"] = [[Due to a high amount of stuff IRL, |cff9482c9Shadow & Light|r team took a vacation.
 
