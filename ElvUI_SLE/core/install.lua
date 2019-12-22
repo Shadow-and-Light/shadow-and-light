1a644b3fe48e7df960f560fa4830788053dfc0a5
Darth Predator <darthpred@gmail.com>
2019-12-22 19:15:53 +0300

    Fuuuuuuuuuuuuuuuuuuuu

--- ElvUI_SLE/core/install.lua
+++ ElvUI_SLE/core/install.lua
@@ -1,4 +1,4 @@
-﻿-local SLE, T, E, L, V, P, G = unpack(select(2, ...))
+﻿local SLE, T, E, L, V, P, G = unpack(select(2, ...))
 local PI = E:GetModule("PluginInstaller")
 PI.SLE_Auth = ""
 PI.SLE_Word = ""
