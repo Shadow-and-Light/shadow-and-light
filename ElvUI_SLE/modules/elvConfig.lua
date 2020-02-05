586742d3eeefb7f9b463dd330d36a49af6fb29b3
Darth Predator <darthpred@gmail.com>
2020-02-05 20:54:53 +0300

    Fixed options being on fire for unitframes

--- ElvUI_SLE/modules/elvConfig.lua
+++ ElvUI_SLE/modules/elvConfig.lua
@@ -15,7 +15,7 @@ end
 function EC:UpdateActionbars()
 	--Change stuff for regular bars
 	for i=1, 10 do
-		E.Options.args.actionbar.args['bar'..i].args.buttonspacing.min = -4
+		E.Options.args.actionbar.args.playerBars.args["bar"..i].args.buttonspacing.min = -4
 	end
 	E.Options.args.actionbar.args.barPet.args.buttonspacing.min = -4
 	E.Options.args.actionbar.args.stanceBar.args.buttonspacing.min = -4
@@ -24,27 +24,32 @@ end
 --Allowing group frames to have negative minimum on horizontal and vertical spacing. Also max camera distance
 function EC:UpdateUitframes()
 	--Boss/Arena
-	E.Options.args.unitframe.args.boss.args.generalGroup.args.spacing.min = -4
-	E.Options.args.unitframe.args.arena.args.generalGroup.args.spacing.min = -4
+	E.Options.args.unitframe.args.groupUnits.args.boss.args.generalGroup.args.spacing.min = -4
+	E.Options.args.unitframe.args.groupUnits.args.arena.args.generalGroup.args.spacing.min = -4
 	--Party
-	E.Options.args.unitframe.args.party.args.generalGroup.args.positionsGroup.args.horizontalSpacing.min = -4
-	E.Options.args.unitframe.args.party.args.generalGroup.args.positionsGroup.args.verticalSpacing.min = -4
+	E.Options.args.unitframe.args.groupUnits.args.party.args.generalGroup.args.positionsGroup.args.horizontalSpacing.min = -4
+	E.Options.args.unitframe.args.groupUnits.args.party.args.generalGroup.args.positionsGroup.args.verticalSpacing.min = -4
 	--Raid
-	E.Options.args.unitframe.args.raid.args.generalGroup.args.positionsGroup.args.horizontalSpacing.min = -4
-	E.Options.args.unitframe.args.raid.args.generalGroup.args.positionsGroup.args.verticalSpacing.min = -4
+	E.Options.args.unitframe.args.groupUnits.args.raid.args.generalGroup.args.positionsGroup.args.horizontalSpacing.min = -4
+	E.Options.args.unitframe.args.groupUnits.args.raid.args.generalGroup.args.positionsGroup.args.verticalSpacing.min = -4
 	--Raid 40
-	E.Options.args.unitframe.args.raid40.args.generalGroup.args.positionsGroup.args.horizontalSpacing.min = -4
-	E.Options.args.unitframe.args.raid40.args.generalGroup.args.positionsGroup.args.verticalSpacing.min = -4
+	E.Options.args.unitframe.args.groupUnits.args.raid40.args.generalGroup.args.positionsGroup.args.horizontalSpacing.min = -4
+	E.Options.args.unitframe.args.groupUnits.args.raid40.args.generalGroup.args.positionsGroup.args.verticalSpacing.min = -4
 	--Raid Pets
-	E.Options.args.unitframe.args.raidpet.args.generalGroup.args.positionsGroup.args.horizontalSpacing.min = -4
-	E.Options.args.unitframe.args.raidpet.args.generalGroup.args.positionsGroup.args.verticalSpacing.min = -4
+	E.Options.args.unitframe.args.groupUnits.args.raidpet.args.generalGroup.args.positionsGroup.args.horizontalSpacing.min = -4
+	E.Options.args.unitframe.args.groupUnits.args.raidpet.args.generalGroup.args.positionsGroup.args.verticalSpacing.min = -4
 	--Tanks/ASssists
-	E.Options.args.unitframe.args.tank.args.generalGroup.args.verticalSpacing.min = -4
-	E.Options.args.unitframe.args.assist.args.generalGroup.args.verticalSpacing.min = -4
+	E.Options.args.unitframe.args.groupUnits.args.tank.args.generalGroup.args.verticalSpacing.min = -4
+	E.Options.args.unitframe.args.groupUnits.args.assist.args.generalGroup.args.verticalSpacing.min = -4
 	--Camera for frames with portraits
-	for unit, settings in T.pairs(E.Options.args.unitframe.args) do
-		if E.Options.args.unitframe.args[unit].args and E.Options.args.unitframe.args[unit].args.portrait then
-			E.Options.args.unitframe.args[unit].args.portrait.args.camDistanceScale.max = 7
+	for unit, settings in T.pairs(E.Options.args.unitframe.args.individualUnits.args) do
+		if E.Options.args.unitframe.args.individualUnits.args[unit].args and E.Options.args.unitframe.args.individualUnits.args[unit].args.portrait then
+			E.Options.args.unitframe.args.individualUnits.args[unit].args.portrait.args.camDistanceScale.max = 7
+		end
+	end
+	for unit, settings in T.pairs(E.Options.args.unitframe.args.groupUnits.args) do
+		if E.Options.args.unitframe.args.groupUnits.args[unit].args and E.Options.args.unitframe.args.groupUnits.args[unit].args.portrait then
+			E.Options.args.unitframe.args.groupUnits.args[unit].args.portrait.args.camDistanceScale.max = 7
 		end
 	end
 end
