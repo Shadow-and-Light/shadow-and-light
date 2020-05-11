ad1da20303fcc3490e6a8b064d14cf1a9ef3350b
Repooc <tjcooper1982@gmail.com>
2020-05-11 00:32:56 -0400

    messing with enhanced shadow config

--- ElvUI_SLE/options/enhancedshadows_c.lua
+++ ElvUI_SLE/options/enhancedshadows_c.lua
@@ -9,6 +9,7 @@ local function configTable()
 		order = 1,
 		type = "group",
 		name = L["Enhanced Shadows"],
+		childGroups = "tab",
 		get = function(info) return E.db.sle.shadows[info[#info]] end,
 		set = function(info, value) E.db.sle.shadows[info[#info]] = value ES:UpdateShadows() end,
 		args = {
@@ -34,16 +35,15 @@ local function configTable()
 				name = L["Use Class Color"],
 			},
 			size = {
-				order = 2,
+				order = 3,
 				type = 'range',
 				name = L["Size"],
 				min = 2, max = 10, step = 1,
 			},
-			frames = {
+			general = {
 				order = 4,
 				type = "group",
-				name = L["Use shadows on..."],
-				-- guiInline = true,
+				name = L["General"],
 				get = function(info) return E.private.sle.module.shadows[info[#info]] end,
 				set = function(info, value) E.private.sle.module.shadows[info[#info]] = value; E:StaticPopup_Show("PRIVATE_RL") end,
 				args = {
@@ -51,228 +51,266 @@ local function configTable()
 						order = 1,
 						type = "toggle",
 						name = L["Enhanced Vehicle Bar"],
+						-- desc = "Testing inline desc here.",
+						descStyle = "inline",
+					},
+					spacer = {
+						order = 2,
+						type = 'description',
+						name = ""
 					},
 					minimap = {
-						order = 2,
+						order = 3,
 						type = "toggle",
 						name = L["Minimap"],
 					},
-					unitframes = {
-						order = 10,
-						type = "group",
-						name = L["UnitFrames"],
-						args = {
-							player = {
-								order = 1,
-								type = "toggle",
-								name = L["Player Frame"],
-							},
-							playerLegacy = {
-								order = 2,
-								type = "toggle",
-								name = L["Player Frame Classic"],
-							},
-							target = {
-								order = 5,
-								type = "toggle",
-								name = L["Target Frame"],
-							},
-							targetLegacy = {
-								order = 6,
-								type = "toggle",
-								name = L["Target Frame Classic"],
-							},
-							targettarget = {
-								order = 10,
-								type = "toggle",
-								name = L["TargetTarget Frame"],
-							},
-							targettargetLegacy = {
-								order = 11,
-								type = "toggle",
-								name = L["TargetTarget Frame Classic"],
-							},
-							focus = {
-								order = 15,
-								type = "toggle",
-								name = L["Focus Frame"],
-							},
-							focusLegacy = {
-								order = 16,
-								type = "toggle",
-								name = L["Focus Frame Classic"],
-							},
-							focustarget = {
-								order = 20,
-								type = "toggle",
-								name = L["FocusTarget Frame"],
-							},
-							focustargetLegacy = {
-								order = 21,
-								type = "toggle",
-								name = L["FocusTarget Frame Classic"],
-							},
-							pet = {
-								order = 25,
-								type = "toggle",
-								name = L["Pet Frame"],
-							},
-							petLegacy = {
-								order = 26,
-								type = "toggle",
-								name = L["Pet Frame Classic"],
-							},
-							pettarget = {
-								order = 30,
-								type = "toggle",
-								name = L["PetTarget Frame"],
-							},
-							pettargetLegacy = {
-								order = 31,
-								type = "toggle",
-								name = L["PetTarget Frame Classic"],
-							},
-							boss = {
-								order = 35,
-								type = "toggle",
-								name = L["Boss Frames"],
-							},
-							bossLegacy = {
-								order = 36,
-								type = "toggle",
-								name = L["Boss Frames Classic"],
-							},
-							arena = {
-								order = 40,
-								type = "toggle",
-								name = L["Arena Frames"],
-							},
-							arenaLegacy = {
-								order = 41,
-								type = "toggle",
-								name = L["Arena Frames Classic"],
-							},
-						},
-					},
-					actionbars = {
-						order = 11,
-						type = "group",
-						name = L["ActionBars"],
-						get = function(info) return E.private.sle.module.shadows.actionbars[info[#info]] end,
-						set = function(info, value) E.private.sle.module.shadows.actionbars[info[#info]] = value; E:StaticPopup_Show("PRIVATE_RL") end,
-						args = {
-							microbar = {
-								order = 1,
-								type = "toggle",
-								name = L["Micro Bar"],
-							},
-							microbarbuttons = {
-								order = 2,
-								type = "toggle",
-								name = L["SLE_EnhShadows_MicroButtons_Option"],
-							},
-							stancebar = {
-								order = 3,
-								type = "toggle",
-								name = L["Stance Bar"],
-							},
-							stancebarbuttons = {
-								order = 4,
-								type = "toggle",
-								name = L["SLE_EnhShadows_StanceButtons_Option"],
-							},
-							petbar = {
-								order = 5,
-								type = "toggle",
-								name = L["Pet Bar"],
-							},
-							petbarbuttons = {
-								order = 6,
-								type = "toggle",
-								name = L["SLE_EnhShadows_PetButtons_Option"],
-							},
-						},
-					},
-					chat = {
-						order = 12,
-						type = "group",
-						name = L["Chat"],
-						get = function(info) return E.private.sle.module.shadows.chat[info[#info]] end,
-						set = function(info, value) E.private.sle.module.shadows.chat[info[#info]] = value; E:StaticPopup_Show("PRIVATE_RL") end,
-						args = {
-							left = {
-								order = 1,
-								type = "toggle",
-								name = L["Left Chat"],
-							},
-							right = {
-								order = 2,
-								type = "toggle",
-								name = L["Right Chat"],
-							},
-						},
-					},
-					datatexts = {
-						order = 13,
-						type = "group",
-						name = L["DataTexts"],
-						get = function(info) return E.private.sle.module.shadows.datatexts[info[#info]] end,
-						set = function(info, value) E.private.sle.module.shadows.datatexts[info[#info]] = value; E:StaticPopup_Show("PRIVATE_RL") end,
-						args = {
-							leftchat = {
-								order = 1,
-								type = "toggle",
-								name = L["Left Chat"],
-							},
-							rightchat = {
-								order = 2,
-								type = "toggle",
-								name = L["Right Chat"],
-							},
-						},
-					},
-					databars = {
-						order = 13,
-						type = "group",
-						name = L["DataBars"],
-						get = function(info) return E.private.sle.module.shadows.databars[info[#info]] end,
-						set = function(info, value) E.private.sle.module.shadows.databars[info[#info]] = value; E:StaticPopup_Show("PRIVATE_RL") end,
-						args = {
-							honorbar = {
-								order = 1,
-								type = "toggle",
-								name = L["HONOR"],
-							},
-							expbar = {
-								order = 1,
-								type = "toggle",
-								name = L["XPBAR_LABEL"],
-							},
-							repbar = {
-								order = 1,
-								type = "toggle",
-								name = L["REPUTATION"],
-							},
-							azeritebar = {
-								order = 1,
-								type = "toggle",
-								name = L["Azerite Bar"],
-							},
-						},
-					},					
 				},
 			},
+			actionbars = {
+				order = 5,
+				type = "group",
+				name = L["ActionBars"],
+				get = function(info) return E.private.sle.module.shadows.actionbars[info[#info]] end,
+				set = function(info, value) E.private.sle.module.shadows.actionbars[info[#info]] = value; E:StaticPopup_Show("PRIVATE_RL") end,
+				args = {
+					microbar = {
+						order = 1,
+						type = "toggle",
+						name = L["Micro Bar"],
+					},
+					microbarbuttons = {
+						order = 2,
+						type = "toggle",
+						name = L["SLE_EnhShadows_MicroButtons_Option"],
+					},
+					stancebar = {
+						order = 3,
+						type = "toggle",
+						name = L["Stance Bar"],
+					},
+					stancebarbuttons = {
+						order = 4,
+						type = "toggle",
+						name = L["SLE_EnhShadows_StanceButtons_Option"],
+					},
+					petbar = {
+						order = 5,
+						type = "toggle",
+						name = L["Pet Bar"],
+					},
+					petbarbuttons = {
+						order = 6,
+						type = "toggle",
+						name = L["SLE_EnhShadows_PetButtons_Option"],
+					},
+					spacer = {
+						order = 7,
+						type = 'description',
+						name = "",
+					},
+				},
+			},
+			chat = {
+				order = 5,
+				type = "group",
+				name = L["Chat"],
+				get = function(info) return E.private.sle.module.shadows.chat[info[#info]] end,
+				set = function(info, value) E.private.sle.module.shadows.chat[info[#info]] = value; E:StaticPopup_Show("PRIVATE_RL") end,
+				args = {
+					left = {
+						order = 1,
+						type = "toggle",
+						name = L["Left Chat"],
+					},
+					right = {
+						order = 2,
+						type = "toggle",
+						name = L["Right Chat"],
+					},
+				},
+			},
+			datatexts = {
+				order = 5,
+				type = "group",
+				name = L["DataTexts"],
+				get = function(info) return E.private.sle.module.shadows.datatexts[info[#info]] end,
+				set = function(info, value) E.private.sle.module.shadows.datatexts[info[#info]] = value; E:StaticPopup_Show("PRIVATE_RL") end,
+				args = {
+					leftchat = {
+						order = 1,
+						type = "toggle",
+						name = L["Left Chat"],
+					},
+					rightchat = {
+						order = 2,
+						type = "toggle",
+						name = L["Right Chat"],
+					},
+				},
+			},
+			databars = {
+				order = 5,
+				type = "group",
+				name = L["DataBars"],
+				get = function(info) return E.private.sle.module.shadows.databars[info[#info]] end,
+				set = function(info, value) E.private.sle.module.shadows.databars[info[#info]] = value; E:StaticPopup_Show("PRIVATE_RL") end,
+				args = {
+					honorbar = {
+						order = 1,
+						type = "toggle",
+						name = L["HONOR"],
+					},
+					expbar = {
+						order = 1,
+						type = "toggle",
+						name = L["XPBAR_LABEL"],
+					},
+					repbar = {
+						order = 1,
+						type = "toggle",
+						name = L["REPUTATION"],
+					},
+					azeritebar = {
+						order = 1,
+						type = "toggle",
+						name = L["Azerite Bar"],
+					},
+				},
+			},
+			unitframes = {
+				order = 5,
+				type = "group",
+				name = L["UnitFrames"],
+				get = function(info) return E.private.sle.module.shadows.chat[info[#info]] end,
+				set = function(info, value) E.private.sle.module.shadows.chat[info[#info]] = value; E:StaticPopup_Show("PRIVATE_RL") end,
+				args = {
+					player = {
+						order = 1,
+						type = "toggle",
+						name = L["Player Frame"],
+					},
+					playerLegacy = {
+						order = 2,
+						type = "toggle",
+						name = L["Player Frame Classic"],
+					},
+					spacer = {
+						order = 3,
+						type = 'description',
+						name = "",
+						width = "full",
+					},
+					target = {
+						order = 4,
+						type = "toggle",
+						name = L["Target Frame"],
+					},
+					targetLegacy = {
+						order = 5,
+						type = "toggle",
+						name = L["Target Frame Classic"],
+					},
+					spacer = {
+						order = 6,
+						type = 'description',
+						name = "",
+					},
+					targettarget = {
+						order = 10,
+						type = "toggle",
+						name = L["TargetTarget Frame"],
+					},
+					targettargetLegacy = {
+						order = 11,
+						type = "toggle",
+						name = L["TargetTarget Frame Classic"],
+					},
+					focus = {
+						order = 15,
+						type = "toggle",
+						name = L["Focus Frame"],
+					},
+					focusLegacy = {
+						order = 16,
+						type = "toggle",
+						name = L["Focus Frame Classic"],
+					},
+					focustarget = {
+						order = 20,
+						type = "toggle",
+						name = L["FocusTarget Frame"],
+					},
+					focustargetLegacy = {
+						order = 21,
+						type = "toggle",
+						name = L["FocusTarget Frame Classic"],
+					},
+					pet = {
+						order = 25,
+						type = "toggle",
+						name = L["Pet Frame"],
+					},
+					petLegacy = {
+						order = 26,
+						type = "toggle",
+						name = L["Pet Frame Classic"],
+					},
+					pettarget = {
+						order = 30,
+						type = "toggle",
+						name = L["PetTarget Frame"],
+					},
+					pettargetLegacy = {
+						order = 31,
+						type = "toggle",
+						name = L["PetTarget Frame Classic"],
+					},
+					boss = {
+						order = 35,
+						type = "toggle",
+						name = L["Boss Frames"],
+					},
+					bossLegacy = {
+						order = 36,
+						type = "toggle",
+						name = L["Boss Frames Classic"],
+					},
+					arena = {
+						order = 40,
+						type = "toggle",
+						name = L["Arena Frames"],
+					},
+					arenaLegacy = {
+						order = 41,
+						type = "toggle",
+						name = L["Arena Frames Classic"],
+					},
+				},
+			},
+			-- frames = {
+			-- 	order = 4,
+			-- 	type = "group",
+			-- 	name = L["Shadows"],
+			-- 	get = function(info) return E.private.sle.module.shadows[info[#info]] end,
+			-- 	set = function(info, value) E.private.sle.module.shadows[info[#info]] = value; E:StaticPopup_Show("PRIVATE_RL") end,
+			-- 	args = {
+
+
+
+
+					
+			-- 	},
 		},
 	}
 
 	for i = 1, 10 do
-		E.Options.args.sle.args.modules.args.shadows.args.frames.args.actionbars.args["bar"..i] = {
-			order = i + 6,
+		E.Options.args.sle.args.modules.args.shadows.args.actionbars.args["bar"..i] = {
+			order = i + 7,
 			type = "toggle",
 			name = L["Bar "]..i,
 		}
-		E.Options.args.sle.args.modules.args.shadows.args.frames.args.actionbars.args["bar"..i.."buttons"] = {
-			order = i + 6,
+		E.Options.args.sle.args.modules.args.shadows.args.actionbars.args["bar"..i.."buttons"] = {
+			order = i + 7,
 			type = "toggle",
 			name = T.format(L["SLE_EnhShadows_BarButtons_Option"], i),
 		}
