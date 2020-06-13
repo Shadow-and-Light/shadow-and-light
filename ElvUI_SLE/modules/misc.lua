e6ed1571f0912a1edc73e51579765b4c1b7535f0
Repooc <tjcooper1982@gmail.com>
2020-06-13 06:21:07 -0400

    We don't use this since we removed S&L Datatexts, might make it so it dynamically fills custom datatexts as options? idk

--- ElvUI_SLE/modules/misc.lua
+++ ElvUI_SLE/modules/misc.lua
@@ -2,56 +2,55 @@ local SLE, T, E, L, V, P, G = unpack(select(2, ...))
 local M = SLE:NewModule("Misc", 'AceHook-3.0', 'AceEvent-3.0')
 local Tr = E:GetModule('Threat');
 
---GLOBALS: hooksecurefunc, UIParent
+--GLOBALS: unpack, select, hooksecurefunc, UIParent, WorldFrame, CinematicFrame, StopCinematic, CanCancelScene, CancelScene, VehicleExit, _G, ShowUIPanel, HideUIPanel, strfind, RAID_CONTROL, CinematicFrame_CancelCinematic
 local _G = _G
 local ShowUIPanel, HideUIPanel = ShowUIPanel, HideUIPanel
-local IsAddOnLoaded = IsAddOnLoaded
 
---Threat
-function M:ElvUIConfig_OnLoad(event, addon)
-	if addon ~= "ElvUI_OptionsUI" then return end
+-- --Threat
+-- function M:ElvUIConfig_OnLoad(event, addon)
+-- 	if addon ~= "ElvUI_OptionsUI" then return end
 
-	M:Threat_UpdateConfig()
-	M:UnregisterEvent("ADDON_LOADED")
-end
+-- 	M:Threat_UpdateConfig()
+-- 	M:UnregisterEvent("ADDON_LOADED")
+-- end
 
-function M:Threat_UpdatePosition()
-	if not E.db.general.threat.enable or not M.db.threat or not M.db.threat.enable then return end
+-- function M:Threat_UpdatePosition()
+-- 	if not E.db.general.threat.enable or not M.db.threat or not M.db.threat.enable then return end
 
-	Tr.bar:SetInside(M.db.threat.position)
-	Tr.bar:SetParent(M.db.threat.position)
+-- 	Tr.bar:SetInside(M.db.threat.position)
+-- 	Tr.bar:SetParent(M.db.threat.position)
 
-	Tr.bar.text:FontTemplate(nil, E.db.general.threat.textSize, E.db.general.threat.textOutline)
-	Tr.bar:SetFrameStrata('MEDIUM')
-	Tr.bar:SetAlpha(1)
-end
+-- 	Tr.bar.text:FontTemplate(nil, E.db.general.threat.textSize, E.db.general.threat.textOutline)
+-- 	Tr.bar:SetFrameStrata('MEDIUM')
+-- 	Tr.bar:SetAlpha(1)
+-- end
 
-function M:Threat_UpdateConfig()
-	if IsAddOnLoaded("ElvUI_OptionsUI") then
-		if M.db.threat.enable then
-			E.Options.args.general.args.general.args.threatGroup.args.position = {
-				order = 42,
-				name = L["Position"],
-				desc = L["This option have been disabled by Shadow & Light. To return it you need to disable S&L's option. Click here to see it's location."],
-				type = "execute",
-				func = function() E.Libs["AceConfigDialog"]:SelectGroup("ElvUI", "sle") end,
-			}
-		else
-			E.Options.args.general.args.general.args.threatGroup.args.position = {
-				order = 42,
-				type = 'select',
-				name = L["Position"],
-				desc = L["Adjust the position of the threat bar to either the left or right datatext panels."],
-				values = {
-					['LEFTCHAT'] = L["Left Chat"],
-					['RIGHTCHAT'] = L["Right Chat"],
-				},
-				get = function(info) return E.db.general.threat.position end,
-				set = function(info, value) E.db.general.threat.position = value; Tr:UpdatePosition() end,
-			}
-		end
-	end
-end
+-- function M:Threat_UpdateConfig()
+-- 	if IsAddOnLoaded("ElvUI_OptionsUI") then
+-- 		if M.db.threat.enable then
+-- 			E.Options.args.general.args.general.args.threatGroup.args.position = {
+-- 				order = 42,
+-- 				name = L["Position"],
+-- 				desc = L["This option have been disabled by Shadow & Light. To return it you need to disable S&L's option. Click here to see it's location."],
+-- 				type = "execute",
+-- 				func = function() E.Libs["AceConfigDialog"]:SelectGroup("ElvUI", "sle") end,
+-- 			}
+-- 		else
+-- 			E.Options.args.general.args.general.args.threatGroup.args.position = {
+-- 				order = 42,
+-- 				type = 'select',
+-- 				name = L["Position"],
+-- 				desc = L["Adjust the position of the threat bar to either the left or right datatext panels."],
+-- 				values = {
+-- 					['LEFTCHAT'] = L["Left Chat"],
+-- 					['RIGHTCHAT'] = L["Right Chat"],
+-- 				},
+-- 				get = function(info) return E.db.general.threat.position end,
+-- 				set = function(info, value) E.db.general.threat.position = value; Tr:UpdatePosition() end,
+-- 			}
+-- 		end
+-- 	end
+-- end
 
 --Viewports
 function M:SetAllPoints(...)
@@ -145,10 +144,10 @@ function M:Initialize()
 	--Raid Utility
 	if _G["RaidUtility_ShowButton"] then M:RaidUtility_Hook() end
 
-	--Threat
-	hooksecurefunc(Tr, 'UpdatePosition', M.Threat_UpdatePosition)
-	M:RegisterEvent("ADDON_LOADED", "ElvUIConfig_OnLoad")
-	M:Threat_UpdatePosition()
+	-- --Threat
+	-- hooksecurefunc(Tr, 'UpdatePosition', M.Threat_UpdatePosition)
+	-- M:RegisterEvent("ADDON_LOADED", "ElvUIConfig_OnLoad")
+	-- M:Threat_UpdatePosition()
 
 	--Viewport
 	function CinematicFrame_CancelCinematic()
@@ -173,8 +172,8 @@ function M:Initialize()
 
 	function M:ForUpdateAll()
 		M.db = E.db.sle.misc
-		M:Threat_UpdateConfig()
-		M:Threat_UpdatePosition()
+		-- M:Threat_UpdateConfig()
+		-- M:Threat_UpdatePosition()
 		M:SetViewport()
 	end
 end
