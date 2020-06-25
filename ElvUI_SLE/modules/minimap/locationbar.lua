8dadc59b43dccac299f7037ab73a296357e797cd
Repooc <tjcooper1982@gmail.com>
2020-06-25 01:37:01 -0400

    lets condense this down

--- ElvUI_SLE/modules/minimap/locationbar.lua
+++ ElvUI_SLE/modules/minimap/locationbar.lua
@@ -301,13 +301,8 @@ function LP:UpdateCoords(elapsed)
 	--Coords
 
 	if E.MapInfo then
-		local x, y = E.MapInfo.x, E.MapInfo.y
-		if x then x = format(LP.db.format, x * 100) else x = "0" end
-		if y then y = format(LP.db.format, y * 100) else y = "0" end
-		if x == "0" or x == "0.0" or x == "0.00" then x = "-" end
-		if y == "0" or y == "0.0" or y == "0.00" then y = "-" end
-		loc_panel.Xcoord.Text:SetText(x)
-		loc_panel.Ycoord.Text:SetText(y)
+		loc_panel.Xcoord.Text:SetText(format(LP.db.format, E.MapInfo.xText))
+		loc_panel.Ycoord.Text:SetText(format(LP.db.format, E.MapInfo.yText))
 	else
 		loc_panel.Xcoord.Text:SetText("-")
 		loc_panel.Ycoord.Text:SetText("-")
