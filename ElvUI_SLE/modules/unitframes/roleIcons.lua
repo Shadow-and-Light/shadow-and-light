eb57cd3881c730b487e72ba432ce1b93860b18ff
Repooc <tjcooper1982@gmail.com>
2020-07-03 19:13:13 -0400

    test this

--- ElvUI_SLE/modules/unitframes/roleIcons.lua
+++ ElvUI_SLE/modules/unitframes/roleIcons.lua
@@ -1,20 +1,8 @@
-local SLE, T, E, L, V, P, G = unpack(select(2, ...)) 
+local SLE, T, E, L, V, P, G = unpack(select(2, ...))
 local SUF = SLE:GetModule("UnitFrames")
 local UF = E:GetModule('UnitFrames');
 
--- GLOBALS: unpack, select
-local random = random
-
-local function GetBattleFieldIndexFromUnitName(name)
-	local nameFromIndex
-	for index = 1, GetNumBattlefieldScores() do
-		nameFromIndex = GetBattlefieldScore(index)
-		if nameFromIndex == name then
-			return index
-		end
-	end
-	return nil
-end
+-- GLOBALS: unpack, select, random, UnitGroupRolesAssigned, UnitIsConnected, pairs
 
 function SUF:UpdateRoleIcon()
 	local lfdrole = self.GroupRoleIndicator
@@ -25,28 +13,11 @@ function SUF:UpdateRoleIcon()
 		return
 	end
 
-	local isInstance, instanceType = IsInInstance()
-	local role
-	if isInstance and instanceType == "pvp" then
-		local name = GetUnitName(self.unit, true)
-		local index = GetBattleFieldIndexFromUnitName(name)
-		if index then
-			local _, _, _, _, _, _, _, _, classToken, _, _, _, _, _, _, talentSpec = GetBattlefieldScore(index)
-			if classToken and talentSpec then
-				role = SUF.specNameToRole[classToken][talentSpec]
-			else
-				role = UnitGroupRolesAssigned(self.unit) --Fallback
-			end
-		else
-			role = UnitGroupRolesAssigned(self.unit) --Fallback
-		end
-	else
-		role = UnitGroupRolesAssigned(self.unit)
+	local role = UnitGroupRolesAssigned(self.unit)
 		if self.isForced and role == 'NONE' then
 			local rnd = random(1, 3)
 			role = rnd == 1 and "TANK" or (rnd == 2 and "HEALER" or (rnd == 3 and "DAMAGER"))
 		end
-	end
 	if (self.isForced or UnitIsConnected(self.unit)) and ((role == "DAMAGER" and db.damager) or (role == "HEALER" and db.healer) or (role == "TANK" and db.tank)) then
 		lfdrole:SetTexture(SLE.rolePaths[E.db.sle.unitframes.roleicons][role])
 		lfdrole:Show()
