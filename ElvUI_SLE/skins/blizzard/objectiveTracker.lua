c38255df0377f2993770a4be9668050a55d881fc
Repooc <tjcooper1982@gmail.com>
2020-07-31 16:00:30 -0400

    objective tracker error

--- ElvUI_SLE/skins/blizzard/objectiveTracker.lua
+++ ElvUI_SLE/skins/blizzard/objectiveTracker.lua
@@ -120,7 +120,7 @@ local function SkinScenarioButtons()
 	if not block.SLE_Block then
 		block.SLE_Block = CreateFrame("Frame", "ScenarioStageBlock_SLE_Block", block)
 		block.SLE_Block:SetAllPoints(block.NormalBG)
-		block.SLE_Block:SetTemplate("Transparent")
+		block.SLE_Block:CreateBackdrop("Transparent")
 		block.SLE_Block:SetFrameStrata("BACKGROUND")
 
 		block.SLE_Block.Logo = block.SLE_Block:CreateTexture(nil, "OVERLAY")
@@ -133,7 +133,7 @@ local function SkinScenarioButtons()
 		Sk.additionalTextures["ScenarioLogo"] = block.SLE_Block.Logo
 		Sk:UpdateAdditionalTexture(Sk.additionalTextures["ScenarioLogo"], SLE.ScenarioBlockLogos[E.private.sle.skins.objectiveTracker.skinnedTextureLogo] or E.private.sle.skins.objectiveTracker.customTextureLogo)
 	end
-	
+
 	if not E.private.sle.skins.objectiveTracker.scenarioBG then
 		-- pop-up artwork
 		block.NormalBG:Hide()
@@ -195,7 +195,7 @@ local function SkinChallengeModeBlock(timerID, elapsedTime, timeLimit)
 		block.SLE_OverlayFrame.Mark3:SetShown(false)
 		block.SLE_OverlayFrame.Mark2:SetShown(false)
 	end
-	
+
 	if not E.private.sle.skins.objectiveTracker.scenarioBG then
 		for i = 1, block:GetNumRegions() do
 			local region = select(i, block:GetRegions())
@@ -247,7 +247,7 @@ local function UpdateChallengeModeTime(block, elapsedTime)
 		else
 			block.SLE_OverlayFrame.LimitText2:Hide()
 		end
-	elseif elapsedTime < time2 then --2 chest timer 
+	elseif elapsedTime < time2 then --2 chest timer
 		timervalue, formatID, nextUpdate, remainder = E:GetTimeInfo(time2 - elapsedTime, 0, 60, 3600)
 		block.SLE_OverlayFrame.LimitText:SetText(format(E.TimeFormats[formatID][1], timervalue, remainder))
 		block.SLE_OverlayFrame.LimitText:SetTextColor(0.78, 0.78, 0.812)
