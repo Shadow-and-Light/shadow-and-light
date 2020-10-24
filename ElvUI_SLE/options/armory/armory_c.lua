570f817cd4ac473b505fefec2b86b4e1475e1b5f
Repooc <tjcooper1982@gmail.com>
2020-10-24 17:55:23 -0400

    format stuff

--- ElvUI_SLE/options/armory/armory_c.lua
+++ ElvUI_SLE/options/armory/armory_c.lua
@@ -1,7 +1,6 @@
 ﻿local SLE, T, E, L, V, P, G = unpack(select(2, ...))
-local Armory = SLE:GetModule("Armory_Core")
-local M = E:GetModule("Misc")
--- local CA = SLE:GetModule("Armory_Character")
+local Armory = SLE:GetModule('Armory_Core')
+local M = E:GetModule('Misc')
 
 local _G = _G
 local STAT_CATEGORY_ATTRIBUTES = STAT_CATEGORY_ATTRIBUTES
@@ -14,12 +13,12 @@ local function configTable()
 		type = 'group',
 		name = L["Armory Mode"],
 		order = 1,
-		childGroups = "tab",
+		childGroups = 'tab',
 		-- hidden = function() return not E.private.skins.blizzard.enable end,
 		args = {
-			desc = ACH:Description(L["SLE_Armory_Info"].."\n", 1, nil, nil, nil, nil, nil, nil,  function() return not E.private.skins.blizzard.enable or (not E.private.skins.blizzard.character and not E.private.skins.blizzard.inspect) end),
+			desc = ACH:Description(L["SLE_Armory_Info"]..'\n', 1, nil, nil, nil, nil, nil, nil,  function() return not E.private.skins.blizzard.enable or (not E.private.skins.blizzard.character and not E.private.skins.blizzard.inspect) end),
 			CA_enable = {
-				type = "toggle",
+				type = 'toggle',
 				name = L["Character Armory"],
 				order = 10,
 				desc = '',
@@ -27,9 +26,9 @@ local function configTable()
 				get = function() return E.db.sle.armory.character.enable end,
 				set = function(_, value)
 					E.db.sle.armory.character.enable = value;
-					SLE:GetModule("Armory_Character"):ToggleArmory();
-					M:UpdatePageInfo(_G.CharacterFrame, "Character")
-					if not E.db.general.itemLevel.displayCharacterInfo then M:ClearPageInfo(_G.CharacterFrame, "Character") end
+					SLE:GetModule('Armory_Character'):ToggleArmory();
+					M:UpdatePageInfo(_G.CharacterFrame, 'Character')
+					if not E.db.general.itemLevel.displayCharacterInfo then M:ClearPageInfo(_G.CharacterFrame, 'Character') end
 				end,
 			},
 			IA_enable = {
@@ -41,9 +40,9 @@ local function configTable()
 				get = function() return E.db.sle.armory.inspect.enable end,
 				set = function(_, value)
 					E.db.sle.armory.inspect.enable = value;
-					SLE:GetModule("Armory_Inspect"):ToggleArmory();
-					M:UpdatePageInfo(_G.InspectFrame, "Inspect") --Putting this under the elv's option check just breaks the shit out of the frame
-					if not E.db.general.itemLevel.displayInspectInfo then M:ClearPageInfo(_G.InspectFrame, "Inspect") end --Clear the infos if those are actually not supposed to be shown.
+					SLE:GetModule('Armory_Inspect'):ToggleArmory();
+					M:UpdatePageInfo(_G.InspectFrame, 'Inspect') --Putting this under the elv's option check just breaks the shit out of the frame
+					if not E.db.general.itemLevel.displayInspectInfo then M:ClearPageInfo(_G.InspectFrame, 'Inspect') end --Clear the infos if those are actually not supposed to be shown.
 				end,
 			},
 			SA_enable = {
@@ -52,27 +51,27 @@ local function configTable()
 				order = 12,
 				desc = '',
 				hidden = function() return not E.private.skins.blizzard.enable or not E.private.skins.blizzard.character end,
-				disabled = function() return (SLE._Compatibility["DejaCharacterStats"] or not E.private.skins.blizzard.enable) end,
+				disabled = function() return (SLE._Compatibility['DejaCharacterStats'] or not E.private.skins.blizzard.enable) end,
 				get = function() return E.db.sle.armory.stats.enable end,
-				set = function(_, value) E.db.sle.armory.stats.enable = value; SLE:GetModule("Armory_Stats"):ToggleArmory(); end
+				set = function(_, value) E.db.sle.armory.stats.enable = value; SLE:GetModule('Armory_Stats'):ToggleArmory(); end
 			},
 			GoToElv = {
 				order = 100,
-				type = "execute",
-				name = "ElvUI: "..L["Item Level"],
-				func = function() E.Libs["AceConfigDialog"]:SelectGroup("ElvUI", "general", "blizzUIImprovements") end,
+				type = 'execute',
+				name = 'ElvUI: '..L["Item Level"],
+				func = function() E.Libs['AceConfigDialog']:SelectGroup('ElvUI', 'general', 'blizzUIImprovements') end,
 				hidden = function()
 					if not E.private.skins.blizzard.enable then return true end
 					return not (E.private.skins.blizzard.character or E.private.skins.blizzard.inspect)
 				end,
 			},
 			--In case some fucker disabled all the skins
-			SkinsDisabled = ACH:Description("|cffff0000 !!!|r "..L["SLE_Armory_SkinDisabled"].." |cffff0000 !!!|r", 500, "medium", nil, nil, nil, nil, nil, function() if not E.private.skins.blizzard.enable then return false end return (E.private.skins.blizzard.character or E.private.skins.blizzard.inspect) end),
+			SkinsDisabled = ACH:Description('|cffff0000 !!!|r '..L["SLE_Armory_SkinDisabled"]..' |cffff0000 !!!|r', 500, 'medium', nil, nil, nil, nil, nil, function() if not E.private.skins.blizzard.enable then return false end return (E.private.skins.blizzard.character or E.private.skins.blizzard.inspect) end),
 			GoToElv_Skins = {
 				order = 501,
-				type = "execute",
-				name = "ElvUI: "..L["Skins"],
-				func = function() E.Libs["AceConfigDialog"]:SelectGroup("ElvUI", "skins") end,
+				type = 'execute',
+				name = 'ElvUI: '..L["Skins"],
+				func = function() E.Libs['AceConfigDialog']:SelectGroup('ElvUI', 'skins') end,
 				hidden = function()
 					if not E.private.skins.blizzard.enable then return false end
 					return (E.private.skins.blizzard.character or E.private.skins.blizzard.inspect)
