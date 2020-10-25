a9b4779c160fb036ebca3a0609437abd19a7e300
Repooc <tjcooper1982@gmail.com>
2020-10-25 02:10:59 -0400

    update warcampaign config

--- ElvUI_SLE/options/warcampaign_c.lua
+++ ElvUI_SLE/options/warcampaign_c.lua
@@ -1,26 +1,26 @@
-﻿local SLE, T, E, L, V, P, G = unpack(select(2, ...))
+﻿local SLE, _, E, L = unpack(select(2, ...))
 
 local WAR_CAMPAIGN = WAR_CAMPAIGN
 
 local function configTable()
 	if not SLE.initialized then return end
 	E.Options.args.sle.args.modules.args.warcampaign = {
-		type = "group",
+		type = 'group',
 		name = WAR_CAMPAIGN,
 		order = 1,
 		args = {
 			header = E.Libs.ACH:Header(WAR_CAMPAIGN, 1),
 			autoOrder = {
 				order = 2,
-				type = "group",
+				type = 'group',
 				name = L["Auto Work Orders"],
 				guiInline = true,
-				get = function(info) return E.db.sle.legacy.warwampaign.autoOrder[ info[#info] ] end,
-				set = function(info, value) E.db.sle.legacy.warwampaign.autoOrder[ info[#info] ] = value end,
+				get = function(info) return E.db.sle.legacy.warwampaign.autoOrder[info[#info]] end,
+				set = function(info, value) E.db.sle.legacy.warwampaign.autoOrder[info[#info]] = value end,
 				args = {
 					enable = {
 						order = 1,
-						type = "toggle",
+						type = 'toggle',
 						name = L["Enable"],
 						desc = L["Automatically queue maximum number of work orders available when visiting respected NPC."],
 					},
