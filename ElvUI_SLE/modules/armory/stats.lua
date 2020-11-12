092630c7e67a9f83c80d93f46e1307278a0b2988
Repooc <tjcooper1982@gmail.com>
2020-11-12 03:31:16 -0500

    he's redoing his stuff anyways

--- ElvUI_SLE/modules/armory/stats.lua
+++ ElvUI_SLE/modules/armory/stats.lua
@@ -1,5 +1,4 @@
 local SLE, T, E, L, V, P, G = unpack(select(2, ...))
-if select(2, GetAddOnInfo('ElvUI_KnightFrame')) and IsAddOnLoaded('ElvUI_KnightFrame') then return end --Don't break korean code :D
 local Armory = SLE:GetModule("Armory_Core")
 local SA = SLE:NewModule("Armory_Stats") --, "AceEvent-3.0", "AceConsole-3.0", "AceHook-3.0")
 local M = E:GetModule("Misc")
