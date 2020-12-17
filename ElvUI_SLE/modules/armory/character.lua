d58b10c8301403465c06cf190e80fcbe63bb8446
Repooc <tjcooper1982@gmail.com>
2020-12-17 01:20:27 -0500

    bye bye

--- ElvUI_SLE/modules/armory/character.lua
+++ ElvUI_SLE/modules/armory/character.lua
@@ -1,5 +1,4 @@
 local SLE, T, E, L, V, P, G = unpack(select(2, ...))
-if select(2, GetAddOnInfo('ElvUI_KnightFrame')) and IsAddOnLoaded('ElvUI_KnightFrame') then return end --Don't break korean code :D
 local Armory = SLE:GetModule("Armory_Core")
 local CA = SLE:NewModule("Armory_Character", "AceEvent-3.0", "AceConsole-3.0", "AceHook-3.0");
 local LCG = LibStub('LibCustomGlow-1.0')
