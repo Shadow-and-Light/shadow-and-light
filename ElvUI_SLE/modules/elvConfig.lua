417e9f482f6adda2b0ba7cea861efafa3da9e24c
Repooc <tjcooper1982@gmail.com>
2020-12-28 18:26:00 -0500

    fix a misspelling and fix error due to Azil cleaning up AB module in ElvUI

--- ElvUI_SLE/modules/elvConfig.lua
+++ ElvUI_SLE/modules/elvConfig.lua
@@ -4,13 +4,13 @@ local EC = SLE:NewModule('ElvConfig', 'AceEvent-3.0')
 function EC:UpdateActionbars()
 	-- Change Actionbars min button spacing
 	for i=1, 10 do
-		E.Options.args.actionbar.args.playerBars.args['bar'..i].args.buttonGroup.args.buttonspacing.min = -4
+		E.Options.args.actionbar.args.playerBars.args['bar'..i].args.buttonGroup.args.buttonSpacing.min = -4
 	end
-	E.Options.args.actionbar.args.barPet.args.buttonGroup.args.buttonspacing.min = -4
-	E.Options.args.actionbar.args.stanceBar.args.buttonGroup.args.buttonspacing.min = -4
+	E.Options.args.actionbar.args.barPet.args.buttonGroup.args.buttonSpacing.min = -4
+	E.Options.args.actionbar.args.stanceBar.args.buttonGroup.args.buttonSpacing.min = -4
 end
 
-function EC:UpdateUitframes()
+function EC:UpdateUnitframes()
 	-- Change Group frames min spacing
 	E.Options.args.unitframe.args.groupUnits.args.boss.args.generalGroup.args.positionsGroup.args.spacing.min = -4
 	E.Options.args.unitframe.args.groupUnits.args.arena.args.generalGroup.args.positionsGroup.args.spacing.min = -4
@@ -42,7 +42,7 @@ function EC:ADDON_LOADED(event, addon)
 	if addon ~= 'ElvUI_OptionsUI' then return end
 	EC:UnregisterEvent(event)
 	EC:UpdateActionbars()
-	EC:UpdateUitframes()
+	EC:UpdateUnitframes()
 end
 
 function EC:Initialize()
