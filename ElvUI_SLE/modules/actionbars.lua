0ef4e9f69075bb0fe0fa485a8001e02b07c6f1d5
Repooc <tjcooper1982@gmail.com>
2022-02-25 21:22:02 -0500

    a better way to do the same thing and not crash the client if using ActionBar Masks (ElvUI Plugin)

--- ElvUI_SLE/modules/actionbars.lua
+++ ElvUI_SLE/modules/actionbars.lua
@@ -6,31 +6,19 @@ local AB = E.ActionBars
 local hooksecurefunc = hooksecurefunc
 local NUM_ACTIONBAR_BUTTONS = NUM_ACTIONBAR_BUTTONS
 
-A.CheckedTextures = {}
-
 function A:Initialize()
 	if not SLE.initialized or E.private.actionbar.enable ~= true then return end
-	A.MaxBars = 10 --In case ExtraActionBars is enabled. Cause 7+ bars will not be affected otherwise
 
 	--Creating checked textures on actionbars
 	if E.private.sle.actionbars.checkedtexture and not (LibStub('Masque', true) and E.private.actionbar.masque.actionbars) then
 		hooksecurefunc(AB, 'PositionAndSizeBar', function(_, barName)
 			local bar = AB['handledBars'][barName]
-			if not A.CheckedTextures[barName] then A.CheckedTextures[barName] = {} end
 
 			for i=1, NUM_ACTIONBAR_BUTTONS do
 				local button = bar.buttons[i]
-
-				if button.SetCheckedTexture then
-					if not A.CheckedTextures[barName][i] then
-						A.CheckedTextures[barName][i] = button:CreateTexture(button:GetName()..'CheckedTexture', 'OVERLAY')
-					end
-
+				if button.checked and button.checked.SetColorTexture then
 					local color = E.private.sle.actionbars.checkedColor
-					button.checked = A.CheckedTextures[barName][i]
 					button.checked:SetColorTexture(color.r, color.g, color.b, color.a)
-					button.checked:SetInside()
-					button:SetCheckedTexture(A.CheckedTextures[barName][i])
 				end
 			end
 		end)
