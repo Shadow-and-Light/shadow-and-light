30bc33451f53ef59e04854db425ed2d37b20ea92
Repooc <tjcooper1982@gmail.com>
2022-10-21 19:14:33 -0400

    added Mechagon and Karazhan portals

--- ElvUI_SLE/modules/minimap/locationbar.lua
+++ ElvUI_SLE/modules/minimap/locationbar.lua
@@ -223,32 +223,34 @@ LP.Spells = {
 		},
 	},
 	['challenge'] = {
-		[1] = {text = GetSpellInfo(131204),icon = SLE:GetIconFromID('spell', 131204),secure = {buttonType = 'spell',ID = 131204}, UseTooltip = true},-- Jade serpent
-		[2] = {text = GetSpellInfo(131205),icon = SLE:GetIconFromID('spell', 131205),secure = {buttonType = 'spell',ID = 131205}, UseTooltip = true}, -- Brew
-		[3] = {text = GetSpellInfo(131206),icon = SLE:GetIconFromID('spell', 131206),secure = {buttonType = 'spell',ID = 131206}, UseTooltip = true},-- Shado-pan
-		[4] = {text = GetSpellInfo(131222),icon = SLE:GetIconFromID('spell', 131222),secure = {buttonType = 'spell',ID = 131222}, UseTooltip = true},-- Mogu
-		[5] = {text = GetSpellInfo(131225),icon = SLE:GetIconFromID('spell', 131225),secure = {buttonType = 'spell',ID = 131225}, UseTooltip = true},-- Setting sun
-		[6] = {text = GetSpellInfo(131231),icon = SLE:GetIconFromID('spell', 131231),secure = {buttonType = 'spell',ID = 131231}, UseTooltip = true},-- Scarlet blade
-		[7] = {text = GetSpellInfo(131229),icon = SLE:GetIconFromID('spell', 131229),secure = {buttonType = 'spell',ID = 131229}, UseTooltip = true},-- scarlet mitre
-		[8] = {text = GetSpellInfo(131232),icon = SLE:GetIconFromID('spell', 131232),secure = {buttonType = 'spell',ID = 131232}, UseTooltip = true},-- Scholo
-		[9] = {text = GetSpellInfo(131228),icon = SLE:GetIconFromID('spell', 131228),secure = {buttonType = 'spell',ID = 131228}, UseTooltip = true},-- Black ox
-		[10] = {text = GetSpellInfo(159895),icon = SLE:GetIconFromID('spell', 159895),secure = {buttonType = 'spell',ID = 159895}, UseTooltip = true},-- bloodmaul
-		[11] = {text = GetSpellInfo(159902),icon = SLE:GetIconFromID('spell', 159902),secure = {buttonType = 'spell',ID = 159902}, UseTooltip = true},-- burning mountain
-		[12] = {text = GetSpellInfo(159899),icon = SLE:GetIconFromID('spell', 159899),secure = {buttonType = 'spell',ID = 159899}, UseTooltip = true},-- crescent moon
-		[13] = {text = GetSpellInfo(159900),icon = SLE:GetIconFromID('spell', 159900),secure = {buttonType = 'spell',ID = 159900}, UseTooltip = true},-- dark rail
-		[14] = {text = GetSpellInfo(159896),icon = SLE:GetIconFromID('spell', 159896),secure = {buttonType = 'spell',ID = 159896}, UseTooltip = true},-- iron prow
-		[15] = {text = GetSpellInfo(159898),icon = SLE:GetIconFromID('spell', 159898),secure = {buttonType = 'spell',ID = 159898}, UseTooltip = true},-- Skies
-		[16] = {text = GetSpellInfo(159901),icon = SLE:GetIconFromID('spell', 159901),secure = {buttonType = 'spell',ID = 159901}, UseTooltip = true},-- Verdant
-		[17] = {text = GetSpellInfo(159897),icon = SLE:GetIconFromID('spell', 159897),secure = {buttonType = 'spell',ID = 159897}, UseTooltip = true},-- Vigilant
-		[18] = {text = GetSpellInfo(354468),icon = SLE:GetIconFromID('spell', 354468),secure = {buttonType = 'spell',ID = 354468}, UseTooltip = true},-- De Other Side
-		[19] = {text = GetSpellInfo(354465),icon = SLE:GetIconFromID('spell', 354465),secure = {buttonType = 'spell',ID = 354465}, UseTooltip = true},-- Halls of Atonement
-		[20] = {text = GetSpellInfo(354464),icon = SLE:GetIconFromID('spell', 354464),secure = {buttonType = 'spell',ID = 354464}, UseTooltip = true},-- Mists of Tirna Scithe
-		[21] = {text = GetSpellInfo(354463),icon = SLE:GetIconFromID('spell', 354463),secure = {buttonType = 'spell',ID = 354463}, UseTooltip = true},-- Plaguefall
-		[22] = {text = GetSpellInfo(354469),icon = SLE:GetIconFromID('spell', 354469),secure = {buttonType = 'spell',ID = 354469}, UseTooltip = true},-- Sanguine Depths
-		[23] = {text = GetSpellInfo(354466),icon = SLE:GetIconFromID('spell', 354466),secure = {buttonType = 'spell',ID = 354466}, UseTooltip = true},-- Spires of Ascension
-		[24] = {text = GetSpellInfo(354462),icon = SLE:GetIconFromID('spell', 354462),secure = {buttonType = 'spell',ID = 354462}, UseTooltip = true},-- Necrotic Wake
-		[25] = {text = GetSpellInfo(354467),icon = SLE:GetIconFromID('spell', 354467),secure = {buttonType = 'spell',ID = 354467}, UseTooltip = true},-- Theater of Pain
-		[26] = {text = GetSpellInfo(367416),icon = SLE:GetIconFromID('spell', 367416),secure = {buttonType = 'spell',ID = 367416}, UseTooltip = true},-- Tazavesh
+		[1] = {text = GetSpellInfo(131204),icon = SLE:GetIconFromID('spell', 131204),secure = {buttonType = 'spell',ID = 131204}, UseTooltip = true},	-- Jade serpent
+		[2] = {text = GetSpellInfo(131205),icon = SLE:GetIconFromID('spell', 131205),secure = {buttonType = 'spell',ID = 131205}, UseTooltip = true},	-- Brew
+		[3] = {text = GetSpellInfo(131206),icon = SLE:GetIconFromID('spell', 131206),secure = {buttonType = 'spell',ID = 131206}, UseTooltip = true},	-- Shado-pan
+		[4] = {text = GetSpellInfo(131222),icon = SLE:GetIconFromID('spell', 131222),secure = {buttonType = 'spell',ID = 131222}, UseTooltip = true},	-- Mogu
+		[5] = {text = GetSpellInfo(131225),icon = SLE:GetIconFromID('spell', 131225),secure = {buttonType = 'spell',ID = 131225}, UseTooltip = true},	-- Setting sun
+		[6] = {text = GetSpellInfo(131231),icon = SLE:GetIconFromID('spell', 131231),secure = {buttonType = 'spell',ID = 131231}, UseTooltip = true},	-- Scarlet blade
+		[7] = {text = GetSpellInfo(131229),icon = SLE:GetIconFromID('spell', 131229),secure = {buttonType = 'spell',ID = 131229}, UseTooltip = true},	-- scarlet mitre
+		[8] = {text = GetSpellInfo(131232),icon = SLE:GetIconFromID('spell', 131232),secure = {buttonType = 'spell',ID = 131232}, UseTooltip = true},	-- Scholo
+		[9] = {text = GetSpellInfo(131228),icon = SLE:GetIconFromID('spell', 131228),secure = {buttonType = 'spell',ID = 131228}, UseTooltip = true},	-- Black ox
+		[10] = {text = GetSpellInfo(159895),icon = SLE:GetIconFromID('spell', 159895),secure = {buttonType = 'spell',ID = 159895}, UseTooltip = true},	-- bloodmaul
+		[11] = {text = GetSpellInfo(159902),icon = SLE:GetIconFromID('spell', 159902),secure = {buttonType = 'spell',ID = 159902}, UseTooltip = true},	-- burning mountain
+		[12] = {text = GetSpellInfo(159899),icon = SLE:GetIconFromID('spell', 159899),secure = {buttonType = 'spell',ID = 159899}, UseTooltip = true},	-- crescent moon
+		[13] = {text = GetSpellInfo(159900),icon = SLE:GetIconFromID('spell', 159900),secure = {buttonType = 'spell',ID = 159900}, UseTooltip = true},	-- dark rail
+		[14] = {text = GetSpellInfo(159896),icon = SLE:GetIconFromID('spell', 159896),secure = {buttonType = 'spell',ID = 159896}, UseTooltip = true},	-- iron prow
+		[15] = {text = GetSpellInfo(159898),icon = SLE:GetIconFromID('spell', 159898),secure = {buttonType = 'spell',ID = 159898}, UseTooltip = true},	-- Skies
+		[16] = {text = GetSpellInfo(159901),icon = SLE:GetIconFromID('spell', 159901),secure = {buttonType = 'spell',ID = 159901}, UseTooltip = true},	-- Verdant
+		[17] = {text = GetSpellInfo(159897),icon = SLE:GetIconFromID('spell', 159897),secure = {buttonType = 'spell',ID = 159897}, UseTooltip = true},	-- Vigilant
+		[18] = {text = GetSpellInfo(354468),icon = SLE:GetIconFromID('spell', 354468),secure = {buttonType = 'spell',ID = 354468}, UseTooltip = true},	-- De Other Side
+		[19] = {text = GetSpellInfo(354465),icon = SLE:GetIconFromID('spell', 354465),secure = {buttonType = 'spell',ID = 354465}, UseTooltip = true},	-- Halls of Atonement
+		[20] = {text = GetSpellInfo(354464),icon = SLE:GetIconFromID('spell', 354464),secure = {buttonType = 'spell',ID = 354464}, UseTooltip = true},	-- Mists of Tirna Scithe
+		[21] = {text = GetSpellInfo(354463),icon = SLE:GetIconFromID('spell', 354463),secure = {buttonType = 'spell',ID = 354463}, UseTooltip = true},	-- Plaguefall
+		[22] = {text = GetSpellInfo(354469),icon = SLE:GetIconFromID('spell', 354469),secure = {buttonType = 'spell',ID = 354469}, UseTooltip = true},	-- Sanguine Depths
+		[23] = {text = GetSpellInfo(354466),icon = SLE:GetIconFromID('spell', 354466),secure = {buttonType = 'spell',ID = 354466}, UseTooltip = true},	-- Spires of Ascension
+		[24] = {text = GetSpellInfo(354462),icon = SLE:GetIconFromID('spell', 354462),secure = {buttonType = 'spell',ID = 354462}, UseTooltip = true},	-- Necrotic Wake
+		[25] = {text = GetSpellInfo(354467),icon = SLE:GetIconFromID('spell', 354467),secure = {buttonType = 'spell',ID = 354467}, UseTooltip = true},	-- Theater of Pain
+		[26] = {text = GetSpellInfo(367416),icon = SLE:GetIconFromID('spell', 367416),secure = {buttonType = 'spell',ID = 367416}, UseTooltip = true},	-- Tazavesh
+		[27] = {text = GetSpellInfo(373274),icon = SLE:GetIconFromID('spell', 373274),secure = {buttonType = 'spell',ID = 373274}, UseTooltip = true},	-- Mechagon
+		[28] = {text = GetSpellInfo(373262),icon = SLE:GetIconFromID('spell', 373262),secure = {buttonType = 'spell',ID = 373262}, UseTooltip = true},	-- Karazhan
 	},
 }
 
