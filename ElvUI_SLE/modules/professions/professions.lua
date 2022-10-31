7d2ec9b043a7c072b7144ba77d41641e83b010a9
Darth Predator <darthpred@gmail.com>
2022-10-31 12:26:19 +0300

    Clearing up professions module a bit

--- ElvUI_SLE/modules/professions/professions.lua
+++ ElvUI_SLE/modules/professions/professions.lua
@@ -4,9 +4,29 @@ local Pr = SLE.Professions
 --GLOBALS: unpack, select, LoadAddOn, IsAddOnLoaded
 local _G = _G
 local GetSpellInfo, IsSpellKnown, GetProfessions, GetProfessionInfo = GetSpellInfo, IsSpellKnown, GetProfessions, GetProfessionInfo
+local IsNPCCrafting = C_TradeSkillUI.IsNPCCrafting
+local IsTradeSkillGuild = C_TradeSkillUI.IsTradeSkillGuild
+local IsTradeSkillGuildMember = C_TradeSkillUI.IsTradeSkillGuildMember
+local IsTradeSkillLinked = C_TradeSkillUI.IsTradeSkillLinked
 
+Pr.baseTradeSkills = {
+	["FirstAid"] = 129,
+	["Blacksmithing"] = 164,
+	["Leatherworking"] = 165,
+	["Alchemy"] = 171,
+	["Herbalism"] = 182,
+	["Cooking"] = 185,
+	["Mining"] = 186,
+	["Tailoring"] = 197,
+	["Engineering"] = 202,
+	["Enchanting"] = 333,
+	["Fishing"] = 356,
+	["Skinning"] = 393,
+	["Jewelcrafting "] = 755,
+	["Inscription"] = 773,
+	["Archeology"] = 794,
+}
 
--- Pr.DEname, Pr.LOCKname, Pr.SMITHname = false, false, false
 function Pr:UpdateSkills(event)
 	if event ~= "CHAT_MSG_SKILL" then
 		Pr.DEname, Pr.LOCKname, Pr.SMITHname = false, false, false
@@ -16,20 +36,18 @@ function Pr:UpdateSkills(event)
 		if(IsSpellKnown(31252)) then Pr.PROSPECTname = GetSpellInfo(31252) end --Jewelcrating
 		if(IsSpellKnown(51005)) then Pr.MILLname = GetSpellInfo(51005) end --Milling
 	end
+end
 
-	local prof1, prof2 = GetProfessions()
-	if prof1 then
-		local name, _, rank = GetProfessionInfo(prof1)
-		if name == GetSpellInfo(7411) then
-			Pr.DErank = rank
-		end
-	end
-	if prof2 then
-		local name, _, rank = GetProfessionInfo(prof2)
-		if name == GetSpellInfo(7411) then
-			Pr.DErank = rank
-		end
-	end
+function Pr:IsSkillMine()
+	local npc = IsNPCCrafting()
+	if npc then return false end
+	local guild = IsTradeSkillGuild()
+	if guild then return false end
+	local member = IsTradeSkillGuildMember()
+	if member then return false end
+	local linked = IsTradeSkillLinked()
+	if linked then return false end
+	return true
 end
 
 function Pr:Initialize()
@@ -39,13 +57,6 @@ function Pr:Initialize()
 	--Next line is to fix other guys' code cause they feel like being assholes and morons
 	-- if SLE._Compatibility["TradeSkillMaster"] and not TradeSkillFrame.RecipeList.collapsedCategories then TradeSkillFrame.RecipeList.collapsedCategories = {} end
 	Pr:UpdateSkills()
-	-- _G["TradeSkillFrame"]:HookScript("OnShow", function(frame)
-	-- 	if Pr.FirstOpen then return end
-	-- 	E:Delay(0.2, function()
-	-- 		Pr.FirstOpen = true
-	-- 		frame.RecipeList.scrollBar:SetValue(0)
-	-- 	end)
-	-- end)
 
 	if E.private.sle.professions.enchant.enchScroll then Pr:EnchantButton() end
 
