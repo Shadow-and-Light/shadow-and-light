d589679cb8c0c562b44298c22a3bdf9afdb24f16
Repooc <tjcooper1982@gmail.com>
2023-06-06 02:36:24 -0400

    dont need any images for the class ones, just use the dressingroom background for each class like this :D

--- ElvUI_SLE/modules/armory/inspect.lua
+++ ElvUI_SLE/modules/armory/inspect.lua
@@ -137,7 +137,7 @@ function IA:Update_BG()
 	elseif E.db.sle.armory.inspect.background.selectedBG == 'CUSTOM' then
 		_G.InspectPaperDollFrame.SLE_Armory_BG:SetTexture(E.db.sle.armory.inspect.background.customTexture)
 	elseif E.db.sle.armory.inspect.background.selectedBG == 'CLASS' then
-		_G.InspectPaperDollFrame.SLE_Armory_BG:SetTexture([[Interface\AddOns\ElvUI_SLE\media\textures\armory\]]..E.myclass)
+		_G.InspectPaperDollFrame.SLE_Armory_BG:SetTexture([[Interface\DRESSUPFRAME\dressingroom]]..strlower(E.myclass))
 	elseif E.db.sle.armory.inspect.background.selectedBG == 'Covenant' then
 		local covenant = SLE.ArmoryConfigBackgroundValues.Covenants[C_Covenants.GetActiveCovenantID()]
 		local bgtexture = SLE:TextureExists([[Interface\AddOns\ElvUI_SLE\media\textures\armory\Cov_]]..covenant) and [[Interface\AddOns\ElvUI_SLE\media\textures\armory\Cov_]]..covenant or nil
