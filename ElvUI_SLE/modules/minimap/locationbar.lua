8a9132d7a0acbba90c5cd37366bb152f45e6b3fb
Darth Predator <darthpred@gmail.com>
2025-03-16 21:00:52 +0300

    Timewalker's Hearthstone

--- ElvUI_SLE/modules/minimap/locationbar.lua
+++ ElvUI_SLE/modules/minimap/locationbar.lua
@@ -104,6 +104,7 @@ LP.Hearthstones = {
 	{209035, nil, true}, -- Hearthstone of the Flame
 	{212337, nil, true}, -- Stone of the Hearth (Hearthstone 10th Anniversary)
 	{210455, nil, true}, -- Draenic Hologem
+	{193588, nil, true}, -- Timewalker's Hearthstone
 }
 
 LP.PortItems = {
