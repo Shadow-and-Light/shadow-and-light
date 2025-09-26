b34aadfbe66789fdedc72a74d17bd20cbe67132f
Repooc <tjcooper1982@gmail.com>
2025-09-26 10:02:34 -0400

    update slerror cmd

--- ElvUI_SLE/core/commands.lua
+++ ElvUI_SLE/core/commands.lua
@@ -10,28 +10,38 @@ local EnableAddOn = C_AddOns.EnableAddOn
 local GetNumAddOns = C_AddOns.GetNumAddOns
 local GetAddOnInfo = C_AddOns.GetAddOnInfo
 
+SL_Addons = {
+	ElvUI_SLE = true,
+	ElvUI_SLE_Dev = true,
+}
+
 function SLE:LuaError(msg)
 	local switch = strlower(msg)
 	if switch == 'on' or switch == '1' then
-		for i=1, GetNumAddOns() do
+		local elvuiaddons = E.Status_Addons
+		local sladdons = E.Status_Addons
+		local bugsack = E.Status_Bugsack
+
+		for i = 1, GetNumAddOns() do
 			local name = GetAddOnInfo(i)
-			if (name ~= 'ElvUI' and name ~= 'ElvUI_Options' and name ~= 'ElvUI_Libraries' and name ~= 'ElvUI_SLE' and name ~= 'ElvUI_SLE_Dev') and E:IsAddOnEnabled(name) then
-				DisableAddOn(name, E.myname)
+			if (not elvuiaddons[name] and not sladdons[name] and (switch == '1' or not bugsack[name])) and E:IsAddOnEnabled(name) then
+				DisableAddOn(name, E.myguid)
 				ElvDB.SLErrorDisabledAddOns[name] = i
 			end
 		end
 
-		SetCVar('scriptErrors', 1)
+		E:SetCVar('scriptErrors', 1)
 		ReloadUI()
 	elseif switch == 'off' or switch == 'devoff' or switch == '0' then
 		if switch == 'off' then
-			SetCVar('scriptErrors', 0)
+			E:SetCVar('scriptProfile', 0)
+			E:SetCVar('scriptErrors', 0)
 			SLE:Print('Lua errors off.')
 		end
 
 		if next(ElvDB.SLErrorDisabledAddOns) then
 			for name in pairs(ElvDB.SLErrorDisabledAddOns) do
-				EnableAddOn(name, E.myname)
+				EnableAddOn(name, E.myguid)
 			end
 
 			wipe(ElvDB.SLErrorDisabledAddOns)
