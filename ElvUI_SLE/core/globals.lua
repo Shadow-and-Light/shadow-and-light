76967d636ce32a07dbc94a9a1375526a4d5e586e
Repooc <tjcooper1982@gmail.com>
2026-01-05 14:14:59 -0500

    update elvui paths

--- ElvUI_SLE/core/globals.lua
+++ ElvUI_SLE/core/globals.lua
@@ -63,9 +63,9 @@ local classTable = {
 --Role icons
 SLE.rolePaths = {
 	['ElvUI'] = {
-		TANK = [[Interface\AddOns\ElvUI\Core\Media\Textures\Tank]],
-		HEALER = [[Interface\AddOns\ElvUI\Core\Media\Textures\Healer]],
-		DAMAGER = [[Interface\AddOns\ElvUI\Core\Media\Textures\DPS]]
+		TANK = [[Interface\AddOns\ElvUI\Game\Shared\Media\Textures\Tank]],
+		HEALER = [[Interface\AddOns\ElvUI\Game\Shared\Media\Textures\Healer]],
+		DAMAGER = [[Interface\AddOns\ElvUI\Game\Shared\Media\Textures\DPS]]
 	},
 	['SupervillainUI'] = {
 		TANK = [[Interface\AddOns\ElvUI_SLE\media\textures\role\svui-tank]],
